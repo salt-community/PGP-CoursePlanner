e20cd892b2ecc0a68b3b4b331bbbdc78aeac1750
Ewy Rostam <ewy.rost@gmail.com>
2024-06-05 14:04:43 +0200

    Position navbar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <section className="flex">
+    <section className="w-screen h-screen">
       <NavBar />
     </section>
 
--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,7 +1,8 @@
 export default function NavBar() {
 
     return (
-        <>
-        </>
+        <div className="navbar bg-base-300">
+            <button className="btn btn-ghost text-xl">daisyUI</button>
+        </div>
     )
 }
