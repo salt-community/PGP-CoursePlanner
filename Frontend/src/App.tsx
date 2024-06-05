293a925b1e65edc8b9e0ea9b5c833a289dcdd232
Ewy Rostam <ewy.rost@gmail.com>
2024-06-05 13:56:29 +0200

    Create skeleton for navbar component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <section className="flex">
+      <NavBar />
+    </section>
+
   )
 }
 
