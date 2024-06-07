488c84fcb05c75eaa70cbb2f8ccb09b93029bd79
Ewy Rostam <ewy.rost@gmail.com>
2024-06-07 10:20:44 +0200

    Change module inputs to fit the type

--- Frontend/src/components/module/Module.tsx
+++ Frontend/src/components/module/Module.tsx
@@ -4,10 +4,13 @@ export default function Module() {
     return (
         <section className="px-4">
             <form className="flex flex-col gap-4 ">
-                <input type="text" name="templateName" className="input input-bordered w-full input-sm max-w-xs" placeholder="Template name" />
-                <button type="button" className="btn btn-sm max-w-48 btn-primary">+ Add Week</button>
+                <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto flex space-x-8">
+                    <input type="text" name="moduleName" className="input input-bordered w-full input-sm max-w-xs" placeholder="Module name" />
+                    <input type="number" name="numberOfDays"  className="input input-bordered input-sm max-w-xs" placeholder="Number of days"/>
+                    <button type="button" className="btn btn-sm max-w-48 btn-primary">Apply</button>
+                </div>
                 <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto">
-                    <Day/>
+                    <Day />
                 </div>
                 <input type="submit" className="btn btn-sm mt-4 max-w-48 btn-success text-white" value="Create Template" />
             </form>
