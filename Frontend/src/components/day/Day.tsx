408e245444b63c22cd3335ef989a63dca5087eee
Ewy Rostam <ewy.rost@gmail.com>
2024-06-07 13:38:51 +0200

    Move add event button for days without events

--- Frontend/src/components/day/Day.tsx
+++ Frontend/src/components/day/Day.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import Event from '../event/Event'
-import { DayProps} from './Types'
+import { DayProps } from './Types'
 
 
-export default function Day({dayNumber}: DayProps) {
+export default function Day({ dayNumber }: DayProps) {
     const [numOfEvents, setNumOfEvents] = useState<number>(1);
     const [events, setEvents] = useState<number[]>([]);
 
@@ -13,25 +13,42 @@ export default function Day({dayNumber}: DayProps) {
     }
 
     return (
-        <table className="table table-sm">
-            <thead>
-                <tr>
-                    <th>Day {dayNumber}</th>
-                    <th> </th>
-                    <th> </th>
-                    <th> </th>
-                    <th><button type="button" onClick={handleAddEvent} className="btn btn-sm btn-primary"> + Add Event</button></th>
-                </tr>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Start</th>
-                    <th>End</th>
-                </tr>
-            </thead>
-            <tbody>
-                {events.map((i) => <Event key={i}/>)}
-            </tbody>
-        </table>
+        <>
+            <table className="table table-sm">
+                {events.length > 0 ?
+                    <>
+                        <thead>
+                            <tr>
+                                <th>Day {dayNumber}</th>
+                                <th> </th>
+                                <th> </th>
+                                <th> </th>
+                                <th><button type="button" onClick={handleAddEvent} className="btn btn-sm btn-primary"> + Add Event</button></th>
+                            </tr>
+
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Start</th>
+                                <th>End</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {events.map((i) => <Event key={i} />)}
+                        </tbody>
+                    </>
+                    :
+                    <thead>
+                        <tr>
+                            <th>Day {dayNumber}</th>
+                            <th><div className="w-72"></div></th>
+                            <th><div className="w-64"></div></th>
+                            <th><button type="button" onClick={handleAddEvent} className="btn btn-sm btn-primary"> + Add Event</button></th>
+                        </tr>
+
+                    </thead>
+                }
+            </table>
+        </>
     )
 }
