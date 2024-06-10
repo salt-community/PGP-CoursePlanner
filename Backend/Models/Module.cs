51cfd2a1ecb6ddb10a52e3a5bba5b78e27d274d2
Ewy Rostam <ewy.rost@gmail.com>
2024-06-10 15:44:10 +0200

    fix: move event to separate file

--- Backend/Models/Module.cs
+++ Backend/Models/Module.cs
@@ -11,17 +11,8 @@ namespace Backend.Models
 
     public class Day
     {
-        public int DayNumber { get; set;}
+        public int DayNumber { get; set; }
         public string? Description { get; set; }
         public ICollection<Event> Events { get; set; } = [];
-    }
-
-    public class Event
-    {
-        public required string Name {get; set;}
-        public required string StartTime { get; set; }
-        public required string EndTime { get; set; }
-        public string? Description {get; set; }
-        public ICollection<string> Links { get; set; } = [];
     }
 }
