d627fd697fa3c0fa529fd2370c10dd685a94c6c7
Ewy Rostam <ewy.rost@gmail.com>
2024-06-10 16:15:27 +0200

    feat: Add DbContext

--- Backend/Data/DataContext.cs
+++ Backend/Data/DataContext.cs
@@ -1,9 +1,16 @@
+using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Module> Modules {get; set;}
+        public DbSet<Day> Days {get; set;}
+        public DbSet<Event> Events {get; set;}
     }
 }
