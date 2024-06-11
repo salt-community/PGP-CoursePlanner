8e4dd490eef66d27baaacfa9a718de35c899f403
Ewy Rostam <ewy.rost@gmail.com>
2024-06-11 10:36:41 +0200

    feat: add function to post modules from frontend

--- Frontend/src/api/ModuleApi.ts
+++ Frontend/src/api/ModuleApi.ts
@@ -2,8 +2,21 @@ import { Module } from "../components/module/Types";
 
 const BASE_URL = "http://localhost:5268/Modules";
 
-export async function getAllModules () {
-    const response = await fetch(BASE_URL);
-    const data = await response.json();
-    return data as Module[];
+export async function getAllModules() {
+  const response = await fetch(BASE_URL);
+  const data = await response.json();
+  return data as Module[];
+}
+
+export async function postModule(module: Module) {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(module),
+  });
+
+  const data = await response.json();
+  return data;
 }
