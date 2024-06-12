fb92a33c6b4af1b144ad299f274ee9bf3c22aa69
Ewy Rostam <ewy.rost@gmail.com>
2024-06-12 17:05:16 +0200

    feat: print module name on module details page

--- Frontend/src/pages/ModuleDetails.tsx
+++ Frontend/src/pages/ModuleDetails.tsx
@@ -1,17 +1,23 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getModuleById } from "../api/ModuleApi";
+import Page from "../components/Page";
+import { useQuery } from "react-query";
 
-export default function ModuleDetails () {
-    const {pathname} = useLocation();
+export default function ModuleDetails() {
+    const { pathname } = useLocation();
     const pathArray = pathname.split("/");
-    const moduleId = pathArray[pathArray.length -1];
+    const moduleId = pathArray[pathArray.length - 1];
 
-    const {data: module, isLoading, isError} = useQuery({
+    const { data: module, isLoading, isError } = useQuery({
         queryKey: ['modules', moduleId],
         queryFn: () => getModuleById(parseInt(moduleId))
     });
 
     return (
-        <></>
+        <Page>
+            {isLoading && <p>Loading...</p>}
+            {isError && <p>An error occured</p>}
+            {module && module.name}
+        </Page>
     )
 }
