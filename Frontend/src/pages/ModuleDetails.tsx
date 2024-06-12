d4ba176e0ead07da56cfe7d425d2e513a421f035
Ewy Rostam <ewy.rost@gmail.com>
2024-06-12 16:39:01 +0200

    feat: call function to get module by id

--- Frontend/src/pages/ModuleDetails.tsx
+++ Frontend/src/pages/ModuleDetails.tsx
@@ -1,4 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function ModuleDetails () {
+    const {pathname} = useLocation();
+    const pathArray = pathname.split("/");
+    const moduleId = pathArray[pathArray.length -1];
+
+    const {data: module, isLoading, isError} = useQuery({
+        queryKey: ['modules', moduleId],
+        queryFn: () => getModuleById(moduleId)
+    });
+
     return (
         <></>
     )
