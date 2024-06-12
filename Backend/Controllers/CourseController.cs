5f90e5cc1cd60a6825731098a553b4a7239ed9bc
Pinuten <axel.818@gmail.com>
2024-06-12 14:47:10 +0200

    adds GetCourses to courseController

--- Backend/Controllers/CourseController.cs
+++ Backend/Controllers/CourseController.cs
@@ -1,6 +1,8 @@
 
+using Backend.Models;
 using Backend.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers;
 
@@ -15,4 +17,16 @@ public class CourseController : ControllerBase
     {
         _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Course>>> GetCourse()
+    {
+        return await _context.Courses
+        .Include(m => m.Modules)
+        .ThenInclude(t => t.Days)
+        .ThenInclude(w => w.Events).ToListAsync();
+    }
+
+    //[HttpGet("{id}")]
+    
 }
