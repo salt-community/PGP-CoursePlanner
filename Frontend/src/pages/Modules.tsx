5d4394d3f72fccb2e404c293dff866eaef38cb0d
Ewy Rostam <ewy.rost@gmail.com>
2024-06-12 16:04:37 +0200

    feat: show modules in squares on modules page

--- Frontend/src/pages/Modules.tsx
+++ Frontend/src/pages/Modules.tsx
@@ -8,19 +8,18 @@ export default function Modules() {
         queryKey: ['modules'],
         queryFn: getAllModules
     });
-    console.log(data);
 
     return (
-        <>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
             {isLoading && <p>Loading...</p>}
             {isError && <p>An error occured...</p>}
             {data && data.map((module, index) =>
-                <Link to={`/home/module/${module.id}`} key={module.name + index} className="border border-black pb-[100%] relative">
+                <Link to={`/modules/details/${module.id}`} key={module.name + index} className="border border-black pb-[100%] relative">
                     <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
                         {module.name}
                     </div>
                 </Link>
             )}
-        </>
+        </section>
     )
 }
