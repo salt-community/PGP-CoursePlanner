5519263ffbaea0b8b4ea18da9cd65244bef998d7
Ewy Rostam <ewy.rost@gmail.com>
2024-06-13 14:30:08 +0200

    feat: set module name as standard value in module component

--- Frontend/src/components/module/Module.tsx
+++ Frontend/src/components/module/Module.tsx
@@ -8,8 +8,10 @@ import InputSmall from "../inputFields/InputSmall";
 import { useMutation, useQueryClient } from "react-query";
 
 export default function Module({ submitFunction, module }: ModuleProps) {
-    const [days, setDays] = useState<number>(0);
+    const [moduleName, setModuleName] = useState<string>(module.name);
+    const [days, setDays] = useState<number>(module.days.length);
     const [daysOfModule, setDaysOfModule] = useState<DayType[]>(module.days);
+
 
     const handleDays = () => {
         const numOfDays = ([...Array(days).keys()].map(i => i + 1));
@@ -57,8 +59,8 @@ export default function Module({ submitFunction, module }: ModuleProps) {
         <section className="px-4">
             <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
                 <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto flex space-x-8">
-                    <InputSmall type="text" name="moduleName" placeholder="Module name" />
-                    <input type="number" name="numberOfDays" onChange={(e) => setDays(parseInt(e.target.value))} className="input input-bordered input-sm max-w-xs" placeholder="Number of days" />
+                    <InputSmall type="text" name="moduleName" onChange={(e) => setModuleName(e.target.value)} placeholder="Module name" value={moduleName} />
+                    <input type="number" name="numberOfDays" onChange={(e) => setDays(parseInt(e.target.value))} value={days} className="input input-bordered input-sm max-w-xs" placeholder="Number of days" />
                     <PrimaryBtn onClick={handleDays}>Apply</PrimaryBtn>
                 </div>
                 <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto">
