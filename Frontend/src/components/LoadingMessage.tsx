378b037c607f68a1f2742c2ea139a5e222837f57
Ewy Rostam <ewy.rost@gmail.com>
2024-06-13 11:29:21 +0200

    Add component for loading message

---
+++ Frontend/src/components/LoadingMessage.tsx
@@ -0,0 +1,8 @@
+export default function LoadingMessage() {
+    return (
+        <div className="w-full h-full flex items-center justify-center flex-col">
+            <p>Loading...</p>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    )
+}
