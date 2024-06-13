f1739099af736c2a60812bd9e2dd87ca19afc477
Ewy Rostam <ewy.rost@gmail.com>
2024-06-13 10:38:12 +0200

    feat: create details page for module

--- Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf2b597cea7f10e672fe61556edea7e1d0f319b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb92a33c6b4af1b144ad299f274ee9bf3c22aa69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/src/pages/ModuleDetails.tsx
+++ Frontend/src/pages/ModuleDetails.tsx
@@ -13,11 +13,54 @@ export default function ModuleDetails() {
         queryFn: () => getModuleById(parseInt(moduleId))
     });
 
+
     return (
+
         <Page>
             {isLoading && <p>Loading...</p>}
             {isError && <p>An error occured</p>}
-            {module && module.name}
+            {/* {module && module.name} */}
+
+            <section className="w-11/12 mx-auto flex flex-col gap-4">
+                <h1 className="pb-4">{module!.name}</h1>
+                <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto">
+                    <section className="flex items-center flex-col gap-4 px-1 sm:p-0">
+                        {module && module.days.map((day, index) => {
+                            return (
+                                <table className="table  table-sm" key={"day_" + index}>
+                                    <thead>
+                                        <tr>
+                                            <th>Day</th>
+                                            <th>{day.dayNumber}</th>
+                                        </tr>
+                                        <tr>
+                                            <th className="w-1/4">Summary</th>
+                                            <th className="w-1/4">Description</th>
+                                            <th className="w-1/4">Start</th>
+                                            <th className="w-1/4">End</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {day.events.map((event, eventIndex) =>
+                                            <tr key={eventIndex}>
+                                                <td className="w-1/4">{event.name}</td>
+                                                <td className="w-1/4">{event.description}</td>
+                                                <td className="w-1/4">{event.startTime}</td>
+                                                <td className="w-1/4">{event.endTime}</td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            )
+                        })
+                        }
+                    </section>
+                </div>
+                <div className="pt-4 flex gap-4 flex-col sm:flex-row">
+                    <button  className="btn btn-sm py-1 max-w-xs btn-error text-white">Delete Module </button>
+                    <Link to={`/home/editModule/${module!.id}`} className="btn btn-sm py-1 max-w-xs btn-info text-white"> Edit Module </Link>
+                </div>
+            </section>
         </Page>
     )
 }
