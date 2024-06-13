607acc16f25d67ead7405f0178ceefe81166844c
Ewy Rostam <ewy.rost@gmail.com>
2024-06-13 11:25:30 +0200

    feat: get original module for edit module page

--- Frontend/src/pages/EditModule.tsx
+++ Frontend/src/pages/EditModule.tsx
@@ -1,4 +1,15 @@
+import { getModuleById } from "../api/ModuleApi";
+import { getIdFromPath } from "../helpers/helperMethods";
+
 export default function () {
+
+    const moduleId = getIdFromPath();
+    
+    const { data: module, isLoading, isError } = useQuery({
+        queryKey: ['modules', moduleId],
+        queryFn: () => getModuleById(parseInt(moduleId))
+    });
+    
     return (
         <>
         </>
