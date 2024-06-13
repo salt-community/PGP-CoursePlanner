0d38fe6aa4ab0ef36f6923bf928da9256444c67e
Ewy Rostam <ewy.rost@gmail.com>
2024-06-13 11:04:28 +0200

    Add module component to create module page

--- Frontend/src/pages/CreateModule.tsx
+++ Frontend/src/pages/CreateModule.tsx
@@ -1,6 +1,10 @@
+import Page from "../components/Page";
+import Module from "../components/module/Module";
+
 export default function CreateModule () {
     return (
-        <>
-        </>
+        <Page>
+            <Module/>
+        </Page>
     )
 }
