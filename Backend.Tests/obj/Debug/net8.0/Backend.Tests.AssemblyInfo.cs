9e5d6088cd3c86370e0421de2cdc93d16ce620ba
Pinuten <axel.818@gmail.com>
2024-06-14 10:25:59 +0200

    creates new service folder

--- Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b98f066a5f68fd6fcdb71d300589f6bbc29cca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158e5648d89f794371c61745a37a9c4e0bbe6be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
