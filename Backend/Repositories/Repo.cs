6d609135afa74f089db9506fc97897cc003faef7
Pinuten <axel.818@gmail.com>
2024-06-14 10:46:42 +0200

    adds the rest of the functions to repo.cs

--- Backend/Repositories/Repo.cs
+++ Backend/Repositories/Repo.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Linq.Expressions;
 using Backend.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
@@ -32,5 +33,58 @@ public abstract class Repo<TEntity> where TEntity : class
         catch (Exception ex) { Debug.WriteLine(ex.Message); }
         return null!;
 
+    }
+    public async virtual Task<TEntity> GetSpecificAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var item = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            return item ?? null!;
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+
+    }
+
+    public async virtual Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+
+    }
+
+    public async virtual Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var item = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (item != null)
+            {
+                _context.Set<TEntity>().Remove(item);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return false;
+
+    }
+
+    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        try
+        {
+            var item = await _context.Set<TEntity>().AnyAsync(expression);
+            return item;
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return false;
     }
 }
