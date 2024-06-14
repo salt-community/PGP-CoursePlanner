c3f74f501b4969ed8a28cde122a031037c241a00
Pinuten <axel.818@gmail.com>
2024-06-14 10:45:51 +0200

    adds get all to repo

--- Backend/Repositories/Repo.cs
+++ Backend/Repositories/Repo.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics;
 using Backend.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repositores;
-public abstract class Repo<TEntity> where TEntity: class
+public abstract class Repo<TEntity> where TEntity : class
 {
     private readonly DataContext _context;
     protected Repo(DataContext context)
@@ -11,15 +12,25 @@ public abstract class Repo<TEntity> where TEntity: class
         _context = context;
     }
     public async virtual Task<TEntity> CreateAsync(TEntity entity)
+    {
+        try
         {
-            try
-            {
-                _context.Set<TEntity>().Add(entity);
-                await _context.SaveChangesAsync();
-                return entity;
-            }
-            catch (Exception ex) { Debug.WriteLine(ex.Message); }
-            return null!;
-
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
         }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+
+    }
+    public async virtual Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        try
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+
+    }
 }
