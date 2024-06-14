1b2a21b1298221cc2ea0b809dc443b4a31fd9ac0
Ewy Rostam <ewy.rost@gmail.com>
2024-06-14 11:11:04 +0200

    feat: add module repository to repositories

--- Backend/Repositories/Repositories.cs
+++ Backend/Repositories/Repositories.cs
@@ -1,7 +1,36 @@
+using System.Linq.Expressions;
+using Backend.Data;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Backend.Repositores
 {
     public class Repositores
     {
-        //ewy är en fisk.
+        public class ModuleRepo : Repo<Module>
+        {
+            private readonly DataContext _context;
+
+            public ModuleRepo(DataContext context) : base(context)
+            {
+                _context = context;
+            }
+
+            public override async Task<IEnumerable<Module>> GetAllAsync()
+            {
+                return await _context.Modules
+                                            .Include(module => module.Days)
+                                            .ThenInclude(day => day.Events)
+                                            .ToListAsync();
+            }
+
+            public override async Task<Module> GetSpecificAsync(Expression<Func<Module, bool>> predicate)
+            {
+                return await _context.Modules
+                    .Include(module => module.Days)
+                    .ThenInclude(day => day.Events)
+                    .FirstOrDefaultAsync(predicate) ?? null!; 
+            }
+        }
     }
 }
