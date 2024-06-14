2a41bb11ce455202d134d6455614d1a4bd73e188
Pinuten <axel.818@gmail.com>
2024-06-14 11:39:27 +0200

    adds CreateModulesAsync in Services

--- Backend/Services/ModuleService.cs
+++ Backend/Services/ModuleService.cs
@@ -1,6 +1,23 @@
+using Backend.Models;
+using static Backend.Repositores.Repositores;
+
 namespace Backend.Services;
 
 public class ModuleService
 {
+    private readonly ModuleRepo _repo;
     
+    public ModuleService(ModuleRepo repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<Module> CreateModuleAsync(Module module)
+    {
+        return await _repo.CreateAsync(module);   
+    }
+    public async Task<IEnumerable<Module>> GetAllModulesAsync()
+    {
+        return await _repo.GetAllAsync();
+    }
 }
