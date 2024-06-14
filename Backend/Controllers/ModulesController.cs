cab7edf6653c5a73b4fda037973a86edadbd3478
Ewy Rostam <ewy.rost@gmail.com>
2024-06-14 08:59:27 +0200

    feat: add delete module endpoint

--- Backend/Controllers/ModulesController.cs
+++ Backend/Controllers/ModulesController.cs
@@ -54,40 +54,60 @@ public class ModulesController : ControllerBase
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateModule(int id, [FromBody] Module module)
     {
-       if (id != module.Id)
-            {
-                return BadRequest();
-            }
+        if (id != module.Id)
+        {
+            return BadRequest();
+        }
 
-            var moduleToUpdate = await _context.Modules
-            .Include(module => module.Days)
-            .ThenInclude(day => day.Events)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(module => module.Id == id);
+        var moduleToUpdate = await _context.Modules
+        .Include(module => module.Days)
+        .ThenInclude(day => day.Events)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(module => module.Id == id);
 
-            if (moduleToUpdate == null)
-            {
-                return NotFound();
-            }
+        if (moduleToUpdate == null)
+        {
+            return NotFound();
+        }
 
-            var days = await _context.Days.Include(day => day.Events).AsNoTracking().ToListAsync();
+        var days = await _context.Days.Include(day => day.Events).AsNoTracking().ToListAsync();
 
-            var deleteList = moduleToUpdate.Days.Where(day => !module.Days.Any(putDay => day.Id == putDay.Id)).ToList();
+        var deleteList = moduleToUpdate.Days.Where(day => !module.Days.Any(putDay => day.Id == putDay.Id)).ToList();
 
-            foreach (var day in deleteList)
-            {
-                _context.Days.Remove(day);
-                Console.WriteLine(day.Id);
+        foreach (var day in deleteList)
+        {
+            _context.Days.Remove(day);
+            Console.WriteLine(day.Id);
 
-            }
+        }
 
-            moduleToUpdate = module;
-            moduleToUpdate.Days = module.Days;
+        moduleToUpdate = module;
+        moduleToUpdate.Days = module.Days;
 
-            _context.Modules.Update(moduleToUpdate);
+        _context.Modules.Update(moduleToUpdate);
+        await _context.SaveChangesAsync();
+
+
+        return NoContent();
+    }
+
+    [HttpDelete("id")]
+
+    public async Task<IActionResult> DeleteModule(int id)
+    {
+        var moduleToDelete = await _context
+                                        .Modules
+                                        .Include(module => module.Days)
+                                        .ThenInclude(day => day.Events)
+                                        .FirstOrDefaultAsync(module => module.Id == id);
+
+        if (moduleToDelete != null)
+        {
+            _context.Modules.Remove(moduleToDelete);
+
             await _context.SaveChangesAsync();
+        }
 
-
-            return NoContent();
+        return NoContent();
     }
 }
