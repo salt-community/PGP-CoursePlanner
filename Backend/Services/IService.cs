c8f0302459163dd97d965d806019d2520222fca6
Pinuten <axel.818@gmail.com>
2024-06-17 15:26:57 +0200

    updates backend service to new repo file names

--- Backend/Services/IService.cs
+++ Backend/Services/IService.cs
@@ -1,6 +1,6 @@
 using Backend.Models;
 using System.Linq;
-using static Backend.Repositories.Repositories;
+
 
 namespace Backend.Services;
 
--- Backend/Services/ModuleService.cs
+++ Backend/Services/ModuleService.cs
@@ -1,6 +1,6 @@
 using Backend.Models;
 using System.Linq;
-using static Backend.Repositories.Repositories;
+using static Backend.Repositories.SpecificRepo;
 
 namespace Backend.Services;
 
