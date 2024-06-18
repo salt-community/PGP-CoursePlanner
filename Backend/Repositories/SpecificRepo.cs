bb7d39e07cdebf11f4b04cfdfb32881383d550d2
Pinuten <axel.818@gmail.com>
2024-06-18 15:36:44 +0200

    adds working updateAsync in specificRepo

--- Backend/Repositories/SpecificRepo.cs
+++ Backend/Repositories/SpecificRepo.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO.Compression;
 using System.Linq.Expressions;
 using Backend.Data;
 using Backend.Models;
@@ -59,18 +60,43 @@ namespace Backend.Repositories
 
             }
 
-            // public override async Task<Module> UpdateAsync(Module module)
-            // {
-            //     try
-            //     {
-            //         _context.Set<Module>().Update(module);
-            //         await _context.SaveChangesAsync();
-            //         return module;
-            //     }
-            //     catch (Exception ex) { Debug.WriteLine(ex.Message); }
-            //     return null!;
+            public override async Task<Module> UpdateAsync(Module module)
+            {
+                try
+                {
+                    var moduleToUpdate = await _context.Modules
+                        .Include(module => module.Days)
+                        .ThenInclude(day => day.Events)
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(m => m.Id == module.Id);
+                    if (moduleToUpdate == null)
+                    {
+                        return null!;
+                    }
 
-            // }
+                    var allDays = await _context.Days
+                        .Include(day => day.Events)
+                        .AsNoTracking()
+                        .ToListAsync();
+
+                    var deletedList = moduleToUpdate.Days
+                    .Where(day => !module.Days
+                    .Any(d => d.Id == day.Id))
+                    .ToList();
+
+                    foreach (var day in deletedList)
+                    {
+                        _context.Days.Remove(day);
+                    }
+
+                    _context.Set<Module>().Update(module);
+                    await _context.SaveChangesAsync();
+                    return module;
+                }
+                catch (Exception ex) { Debug.WriteLine(ex.Message); }
+                return null!;
+
+            }
         }
     }
 }
