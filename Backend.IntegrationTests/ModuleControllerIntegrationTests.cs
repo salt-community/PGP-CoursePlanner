af7653a134cfb5f35ee65835db4ddb8fad7aace1
Ewy Rostam <ewy.rost@gmail.com>
2024-06-18 11:14:52 +0200

    feat: assert that list has count of 2 - getModules integration test

--- Backend.IntegrationTests/ModuleControllerIntegrationTests.cs
+++ Backend.IntegrationTests/ModuleControllerIntegrationTests.cs
@@ -44,6 +44,7 @@ namespace Backend.IntegrationTests
             // assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             deserializedResponse.Should().NotBeNull();
+            deserializedResponse.Should().HaveCount(2);
 
         }
     }
