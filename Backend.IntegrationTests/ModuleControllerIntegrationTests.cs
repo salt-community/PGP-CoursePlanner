70802f15b00d1b62263c4a36d53b439c069de580
Ewy Rostam <ewy.rost@gmail.com>
2024-06-18 11:02:38 +0200

    feat: add correct database connection to GetModules integration test

--- Backend.IntegrationTests/ModuleControllerIntegrationTests.cs
+++ Backend.IntegrationTests/ModuleControllerIntegrationTests.cs
@@ -1,18 +1,19 @@
+using Backend.Data;
 using Backend.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace Backend.IntegrationTests
 {
     public class ModuleControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Program>>
     {
+        private readonly CustomWebApplicationFactory<Program> _factory;
         private readonly HttpClient _client;
-        private readonly CustomWebApplicationFactory<Program>
-         _factory;
 
-        public ModuleControllerIntegrationTests(
-            CustomWebApplicationFactory<Program> factory)
+        public ModuleControllerIntegrationTests(CustomWebApplicationFactory<Program> factory)
         {
             _factory = factory;
             _client = factory.CreateClient(new WebApplicationFactoryClientOptions
@@ -25,8 +26,14 @@ namespace Backend.IntegrationTests
         public async Task GetModules_Returns_ListOfModules()
         {
             //  arrange
-            // var createResponse = await _client.PostAsync("/api/jokes", null);
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<DataContext>();
 
+                db.Database.EnsureCreated();
+                Seeding.InitializeTestDB(db);
+            }
             // act 
             var getResponse = await _client.GetAsync("/Modules");
 
