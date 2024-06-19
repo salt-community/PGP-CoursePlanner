53b98433bcb21cd46153f9d843aea2d780a3369e
Ewy Rostam <ewy.rost@gmail.com>
2024-06-19 13:34:00 +0200

    fix: adjust style of drop down menu

--- Frontend/src/components/DropDown.tsx
+++ Frontend/src/components/DropDown.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function DropDown({ modules }: Props) {
     return (
         <div className="flex flex-col justify-center">
-            <select className="border border-black rounded-md mt-2" >
+            <select className="border border-gray-300 rounded-lg mt-2 max-w-xs p-1" >
                 {modules.map(moduleName =>
                     <option value={moduleName}>{moduleName}</option>)}
             </select>
