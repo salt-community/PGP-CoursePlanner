32bcb211a9142f0a5e7528338811176df251b409
Ewy Rostam <ewy.rost@gmail.com>
2024-06-19 11:43:01 +0200

    feat: add course component to create course page

--- Frontend/src/pages/coursePages/CreateCourse.tsx
+++ Frontend/src/pages/coursePages/CreateCourse.tsx
@@ -1,6 +1,10 @@
-export default function CreateCourse () {
+import Page from "../../components/Page";
+import Course from "../../components/course/Course";
+
+export default function CreateCourse() {
     return (
-        <>
-        </>
+        <Page>
+            <Course/>
+        </Page>
     )
 }
