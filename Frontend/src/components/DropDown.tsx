6534ca39a3d909dcb3d814aa21ba8c16cabcd0e4
Ewy Rostam <ewy.rost@gmail.com>
2024-06-19 13:27:16 +0200

    fix: remove label from drop down menu component

--- Frontend/src/components/DropDown.tsx
+++ Frontend/src/components/DropDown.tsx
@@ -5,7 +5,6 @@ type Props = {
 export default function DropDown({ modules }: Props) {
     return (
         <div className="flex flex-col justify-center">
-            <label className="underline mt-5">Choose Module</label>
             <select className="border border-black rounded-md mt-2" >
                 {modules.map(moduleName =>
                     <option value={moduleName}>{moduleName}</option>)}
