a40fcac86ca48d4e54555bd201a13a52f67ec19c
Ewy Rostam <ewy.rost@gmail.com>
2024-06-20 10:39:07 +0200

    fix: move id property from dayprops to daytype

--- Frontend/src/components/day/Types.ts
+++ Frontend/src/components/day/Types.ts
@@ -1,13 +1,13 @@
 import { EventType } from "../event/Types";
 
 export type DayType = {
+  id?: number;
   dayNumber: number;
   description: string;
   events: EventType[];
 };
 
 export type DayProps = {
-  id: number;
   day: DayType;
   setDays: React.Dispatch<React.SetStateAction<DayType[]>>;
   days: DayType[];
