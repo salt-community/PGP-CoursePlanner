e2be5e87790c9d1c1c782c13d6f798fd8af735f1
Ewy Rostam <ewy.rost@gmail.com>
2024-06-24 16:14:19 +0200

    add console log

--- Frontend/src/components/course/Course.tsx
+++ Frontend/src/components/course/Course.tsx
@@ -48,6 +48,8 @@ export default function Course() {
             modules: courseModules,
         };
 
+        console.log("course to post: ", newCourse);
+
         postCourse(newCourse);
     }
 
