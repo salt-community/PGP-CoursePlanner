77277184c6629a5df054c6e6741eb7f926bf05b7
Ewy Rostam <ewy.rost@gmail.com>
2024-06-24 15:54:22 +0200

    feat: add create course to course api

--- Frontend/src/api/CourseApi.ts
+++ Frontend/src/api/CourseApi.ts
@@ -1,4 +1,17 @@
-export default function CourseApi () {
+import { Course } from "../components/course/Types";
 
+const BASE_URL = "http://localhost:5268/Courses";
+
+export async function postCourse(course: Course) {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(course),
+    });
   
-}
+    if (!response.ok) {
+      throw new Error("Failed to create course");
+    }
+  }
