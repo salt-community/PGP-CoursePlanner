7e41989b346738b87abec1027fbf3e168a1eff66
Ewy Rostam <ewy.rost@gmail.com>
2024-06-25 11:21:44 +0200

    feat: add weekdays to homepage

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import Page from "../components/Page";
+import { weekDays } from "../helpers/dateHelpers";
 
 export default function Home() {
     return (
         <Page>
-            <section>
-                
+            <section className="flex border border-black rounded-lg h-80 justify-around m-5">
+                {weekDays.map(day =>
+                    <article key={day} className="border border-black rounded-lg w-40 my-4 flex justify-center">
+                        <h1>{day}</h1>
+                    </article>)}
             </section>
         </Page>
     )
