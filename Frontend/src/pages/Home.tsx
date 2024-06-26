404c414b255f8a940e2f0dba1c4ac5ed78f26dec
Ewy Rostam <ewy.rost@gmail.com>
2024-06-26 09:23:28 +0200

    feat: make container of days full width

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <Page>
             <h1 className="text-2xl font-semibold">We Are In Week {getWeek(new Date())}</h1>
-            <section className="flex border border-black rounded-lg h-80 justify-around m-5">
+            <section className="flex border border-black rounded-lg h-80 w-full justify-around m-5">
                 {weekDays.map(day =>
                     <WeekDay key={format(day, 'd')} date={formatDate(day)}/>
                     )}
