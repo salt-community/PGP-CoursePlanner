1340225352e7b875f5b56423c67eca9b094aee12
Ewy Rostam <ewy.rost@gmail.com>
2024-06-26 12:29:40 +0200

    add link to details page from home page

--- Frontend/src/components/weekDay/WeekDay.tsx
+++ Frontend/src/components/weekDay/WeekDay.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { today } from "../../helpers/dateHelpers";
+import { Link } from "react-router-dom";
 
 type Props = {
     date: string;
@@ -12,9 +13,9 @@ export default function WeekDay({ date }: Props) {
     console.log(date + " : " + color);
     console.log("Today = ", today);
     return (
-        <article className={`border ${color} rounded-lg w-40 my-4 flex justify-center`}>
+        <Link to={`details/${date}`} className={`border ${color} rounded-lg w-40 my-4 flex justify-center`}>
             <h1>{format(date, 'EEEE')}</h1>
-        </article>
+        </Link>
 
     )
 }
