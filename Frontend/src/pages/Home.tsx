1d4369ba74aa088303bba64cabbb2cbaf93e8d01
Ewy Rostam <ewy.rost@gmail.com>
2024-06-26 09:19:53 +0200

    feat: add title with weeknumber on start page

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format, getWeek } from "date-fns";
 import Page from "../components/Page";
 import { formatDate, weekDays } from "../helpers/dateHelpers";
 import WeekDay from "../components/weekDay/WeekDay";
@@ -6,6 +6,7 @@ import WeekDay from "../components/weekDay/WeekDay";
 export default function Home() {
     return (
         <Page>
+            <h1 className="text-2xl font-semibold">We Are In Week {getWeek(new Date())}</h1>
             <section className="flex border border-black rounded-lg h-80 justify-around m-5">
                 {weekDays.map(day =>
                     <WeekDay key={format(day, 'd')} date={formatDate(day)}/>
