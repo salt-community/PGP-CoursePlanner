a73e72111bc790817f3e3fb5a1394a6f0f173e85
Ewy Rostam <ewy.rost@gmail.com>
2024-06-27 11:45:28 +0200

    comment out section in page component

--- Frontend/src/components/Page.tsx
+++ Frontend/src/components/Page.tsx
@@ -9,9 +9,9 @@ export default function Page({ children }: Props) {
     return (
         <section className="w-screen h-screen">
             <NavBar />
-            <section className="p-20 flex flex-col items-center">
+            {/* <section className="p-20 flex flex-col items-center"> */}
                 {children}
-            </section>
+            {/* </section> */}
         </section>
     )
 }
