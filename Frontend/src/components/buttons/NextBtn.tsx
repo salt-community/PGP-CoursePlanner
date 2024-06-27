cf0652eb26ebad90724f857c51b0a13d6c041ae2
Ewy Rostam <ewy.rost@gmail.com>
2024-06-27 15:28:49 +0200

    add correct arrows to previous and next button

--- Frontend/src/components/buttons/NextBtn.tsx
+++ Frontend/src/components/buttons/NextBtn.tsx
@@ -5,18 +5,7 @@ type Props = {
 export default function NextBtn({ onClick }: Props) {
     return (
         <button onClick={onClick} className="btn btn-square btn-outline">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
         </button>
     )
 }
