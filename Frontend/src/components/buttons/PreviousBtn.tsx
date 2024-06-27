7cf86e11861624d0012a08f3f65c44dd8b258b4c
Ewy Rostam <ewy.rost@gmail.com>
2024-06-27 16:18:05 +0200

    restructure calendar month view

--- Frontend/src/components/buttons/PreviousBtn.tsx
+++ Frontend/src/components/buttons/PreviousBtn.tsx
@@ -1,13 +1,11 @@
 type Props = {
-    onClick: () => void;
+    onClick?: () => void;
 }
 
 export default function PreviousBtn({ onClick }: Props) {
     return (
-        <button type="button" onClick={onClick} className="btn btn-sm max-w-48 btn-primary">
-            <button onClick={onClick} className="btn btn-square btn-outline">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z" /></svg>
-            </button>
+        <button onClick={onClick} className="btn btn-square btn-outline">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z" /></svg>
         </button>
     )
 
--- Frontend/src/pages/calendar/MonthView.tsx
+++ Frontend/src/pages/calendar/MonthView.tsx
@@ -1,29 +1,38 @@
 import { format } from "date-fns"
-import { allDaysInMonth, daysBeforeMonth, fullWeek, month} from "../../helpers/dateHelpers"
+import { allDaysInMonth, daysBeforeMonth, fullWeek, month } from "../../helpers/dateHelpers"
 import CalendarLine from "../../components/CalendarLine"
+import NextBtn from "../../components/buttons/NextBtn"
+import PreviousBtn from "../../components/buttons/PreviousBtn"
+import Page from "../../components/Page"
 
 export default function MonthView() {
     return (
-        <div className="flex flex-col items-center">
-            <header className="mt-20 mb-5">
-                <h1 className="text-3xl">
-                    {month}
-                </h1>
-            </header>
-            <div className="justify-center w-80 h-64 shadow-xl drop-shadow-2xl break-normal grid grid-cols-7 rounded-md bg-white lg:w-3/5 lg:h-[65vh]">
-                {fullWeek.map(day => (
-                    <div key={format(day, 'E')} className="w-1/7 flex items-center justify-center py-1 px-1">{format(day, 'E')}</div>
-                ))}
-                <CalendarLine />
-                {daysBeforeMonth.map((emptyDayIndex) => (
-                    <button key={format(emptyDayIndex, 'd')} className="w-1/7"></button>
-                ))}
+        <Page>
+            <section className="px-20 flex items-center justify-between">
+                <PreviousBtn />
+                <div className="flex flex-col items-center w-full">
+                    <header className="mt-5 mb-5">
+                        <h1 className="text-3xl">
+                            {month}
+                        </h1>
+                    </header>
+                    <div className="justify-center w-80 h-64 shadow-xl drop-shadow-2xl break-normal grid grid-cols-7 rounded-md bg-white lg:w-3/5 lg:h-[65vh]">
+                        {fullWeek.map(day => (
+                            <div key={format(day, 'E')} className="w-1/7 flex items-center justify-center py-1 px-1">{format(day, 'E')}</div>
+                        ))}
+                        <CalendarLine />
+                        {daysBeforeMonth.map((emptyDayIndex) => (
+                            <button key={format(emptyDayIndex, 'd')} className="w-1/7"></button>
+                        ))}
 
-                {allDaysInMonth.map((thisDate) => {
-                    return <button key={format(thisDate, 'd')}>{format(thisDate, 'd')}</button>
-                })
-                }
-            </div>
-        </div>
+                        {allDaysInMonth.map((thisDate) => {
+                            return <button className="w-1/7" key={format(thisDate, 'd')}>{format(thisDate, 'd')}</button>
+                        })
+                        }
+                    </div>
+                </div>
+                <NextBtn />
+            </section>
+        </Page>
     )
 }
