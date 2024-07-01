9c963de1ac7dbd80b021515aedc2f4e383713cde
Ewy Rostam <ewy.rost@gmail.com>
2024-07-01 11:17:51 +0200

    refactor: break out DateContent to separate file

--- Backend/Models/CalendarDate.cs
+++ Backend/Models/CalendarDate.cs
@@ -6,14 +6,4 @@ namespace Backend.Models
         public DateTime Date { get; set; }
         public List<DateContent> DateContent { get; set; } = [];
     }
-
-    public class DateContent
-    {
-        public int Id { get; set; }
-        public string? ModuleName { get; set; }
-        public int DayOfModule { get; set; }
-        public int TotalDaysInModule { get; set; }
-        public required string CourseName { get; set; }
-        public List<Event> Events { get; set; } = [];
-    }
 }
