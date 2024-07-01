3d0a1dfa2c26ca33225a86e631b694aea502ca53
Ewy Rostam <ewy.rost@gmail.com>
2024-07-01 11:44:12 +0200

    feat: add new models to DB Set

--- Backend/Data/DataContext.cs
+++ Backend/Data/DataContext.cs
@@ -33,6 +33,10 @@ namespace Backend.Data
         public DbSet<Event> Events { get; set; }
         public DbSet<CourseModule> CourseModules { get; set; }
 
+        public DbSet<AppliedCourse> AppliedCourses {get; set;}
+        public DbSet<CalendarDate> CalendarDates {get; set;}
+        public DbSet<DateContent> DateContent {get; set;}
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
