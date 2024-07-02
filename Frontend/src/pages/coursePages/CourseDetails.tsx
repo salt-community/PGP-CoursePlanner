5754daea368572184c3960f6ab499a63e1be9760
Patt1989 <patricia.vanmarlen@appliedtechnology.se>
2024-07-02 14:54:00 +0200

    postsAppliedCourse when applying template in frontend

--- Frontend/src/pages/coursePages/CourseDetails.tsx
+++ Frontend/src/pages/coursePages/CourseDetails.tsx
@@ -7,6 +7,8 @@ import { ModuleType } from "../../sections/module/Types";
 import { getAllModules } from "../../api/ModuleApi";
 import { useState } from "react";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { postAppliedCourse } from "../../api/AppliedCourseApi";
+import { AppliedCourseType } from "../../sections/course/Types";
 
 export default function CourseDetails() {
     const [startDate, setStartDate] = useState<Date>(new Date());
@@ -30,6 +32,15 @@ export default function CourseDetails() {
         var module = allModules?.find(m => m.id == element);
         modules.push(module!)
     });
+
+    const handleApplyTemplate = () => {
+        const appliedCourse: AppliedCourseType = {
+            startDate: startDate,
+            courseId: parseInt(courseId)
+        };
+        postAppliedCourse(appliedCourse);
+        navigate('/calendar/month')
+    }
 
     const queryClient = useQueryClient();
 
@@ -98,9 +109,9 @@ export default function CourseDetails() {
                         }
                     } />
                     <div className="pt-4 flex gap-4 flex-col sm:flex-row">
-                        <button className="btn btn-sm py-1 max-w-xs btn-success text-white">Apply Template </button>
                         <button onClick={() => mutation.mutate(parseInt(courseId))} className="btn btn-sm py-1 max-w-xs btn-error text-white">Delete Course</button>
                         <Link to={`/courses/edit/${courseId}`} className="btn btn-sm py-1 max-w-xs btn-info text-white">Edit Course</Link>
+                        <button onClick={handleApplyTemplate} className="btn btn-sm py-1 max-w-xs btn-success text-white">Apply Template </button>
                     </div>
                 </section >
             }
