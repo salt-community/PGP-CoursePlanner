006c9b0ffb7c62e288a2b0f20bcbcfe0cdbc3c00
Ewy Rostam <ewy.rost@gmail.com>
2024-07-02 15:52:21 +0200

    return response as calendarDateType when fetching calendar date

--- Frontend/src/api/CalendarDateApi.ts
+++ Frontend/src/api/CalendarDateApi.ts
@@ -1,7 +1,9 @@
+import { CalendarDateType } from "../components/calendar/Types";
+
 const BASE_URL = "http://localhost:5268/CalendarDates";
 
 export async function getCalendarDate(date: Date) {
   const response = await fetch(`${BASE_URL}/${date}`);
   const data = await response.json();
-  //   return data as CourseType;
+    return data as CalendarDateType;
 }
