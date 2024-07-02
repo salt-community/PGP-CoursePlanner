2c03a8372632ac3dafd5431f668578ded47e7635
Ewy Rostam <ewy.rost@gmail.com>
2024-07-02 16:45:00 +0200

    fetches CalendarDate correctly

--- Frontend/src/api/CalendarDateApi.ts
+++ Frontend/src/api/CalendarDateApi.ts
@@ -2,8 +2,11 @@ import { CalendarDateType } from "../components/calendar/Types";
 
 const BASE_URL = "http://localhost:5268/CalendarDates";
 
-export async function getCalendarDate(date: Date) {
+export async function getCalendarDate(date: string) {
   const response = await fetch(`${BASE_URL}/${date}`);
+  console.log("URL", `${BASE_URL}/${date}`)
   const data = await response.json();
     return data as CalendarDateType;
 }
+
+// http://localhost:5268/CalendarDates/Tue Jul 02 2024 00:00:00 GMT+0200 (Central European Summer Time)
