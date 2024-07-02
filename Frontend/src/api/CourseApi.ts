57b810e27fc1fae54a9cae3d427ccba9e734c302
Pinuten <axel.818@gmail.com>
2024-07-02 12:01:16 +0200

    final half working cloud build configs(I think)

--- Frontend/src/api/CourseApi.ts
+++ Frontend/src/api/CourseApi.ts
@@ -1,5 +1,10 @@
 import { CourseType } from "../components/course/Types";
 
+//call to env 
+//2 variables 
+//one for baseurl one for port
+//add slash api 
+
 const BASE_URL = "http://localhost:5268/Courses";
 
 export async function postCourse(course: CourseType) {
