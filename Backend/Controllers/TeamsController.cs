1745ef46267e0037e9ab330bf077aacc02f774ad
Pinuten <axel.818@gmail.com>
2024-07-15 13:47:24 +0200

    adds teamsController to administer teams

---
+++ Backend/Controllers/TeamsController.cs
@@ -0,0 +1,26 @@
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Backend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class TeamsController : ControllerBase
+{
+
+    private readonly IService<Teams> _service;
+    public TeamsController(IService<Teams> service)
+    {
+        _service = service;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Teams>>> GetTeams()
+    {
+        var response = await _service.GetAllAsync();
+        return Ok(response);
+    }
+}
--- Backend/Models/Teams.cs
+++ Backend/Models/Teams.cs
@@ -3,6 +3,8 @@ namespace Backend.Models
     public class Teams
     {
         public int Id {get; set;}
-        
+        public string Name {get; set;}
+        public List<string> Emails {get; set;} = [];
+
     }
 }
