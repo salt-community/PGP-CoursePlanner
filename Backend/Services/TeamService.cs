caa19d7b45636d882b3677773c26cdb11b6552d6
Pinuten <axel.818@gmail.com>
2024-07-15 16:21:53 +0200

    adds TeamService.cs

--- Backend/Services/TeamService.cs
+++ Backend/Services/TeamService.cs
@@ -1,2 +1,52 @@
 using Backend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Backend.Services
+{
+    public class TeamService : IService<Team>
+    {
+        private readonly List<Team> _teams = new List<Team>();
+
+        public Task<Team> CreateAsync(Team team)
+        {
+            team.Id = _teams.Count + 1;
+            _teams.Add(team);
+            return Task.FromResult(team);
+        }
+
+        public Task<List<Team>> GetAllAsync()
+        {
+            return Task.FromResult(_teams.ToList());
+        }
+
+        public Task<Team> GetOneAsync(int id)
+        {
+            var team = _teams.FirstOrDefault(t => t.Id == id);
+            return Task.FromResult(team);
+        }
+
+        public Task<Team> UpdateAsync(int id, Team team)
+        {
+            var existingTeam = _teams.FirstOrDefault(t => t.Id == id);
+            if (existingTeam != null)
+            {
+                existingTeam.Name = team.Name;
+                existingTeam.Emails = team.Emails;
+            }
+            return Task.FromResult(existingTeam);
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            var team = _teams.FirstOrDefault(t => t.Id == id);
+            if (team != null)
+            {
+                _teams.Remove(team);
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
