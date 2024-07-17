4442ce0c3ec1fdb7a5a0cf31c0cb4b9f53ef478c
Ewy Rostam <ewy.rost@gmail.com>
2024-07-17 09:55:31 +0200

    replace loading and error message with components

--- Frontend/src/models/course/pages/Courses.tsx
+++ Frontend/src/models/course/pages/Courses.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { getAllCourses } from "../../../api/CourseApi";
 import Page from "../../../components/Page";
 import { Link } from "react-router-dom";
+import LoadingMessage from "../../../components/LoadingMessage";
+import ErrorMessage from "../../../components/ErrorMessage";
 
 export default function Courses() {
 
@@ -13,8 +15,8 @@ export default function Courses() {
     return (
         <Page>
             <section className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 md:px-24 lg:px-56">
-                {isLoading && <p>Loading...</p>}
-                {isError && <p>An error occured...</p>}
+                {isLoading && <LoadingMessage/>}
+                {isError && <ErrorMessage/>}
                 {data &&
                     <Link to={"/courses/create"} className="border border-black bg-primary text-white pb-[100%] relative">
                         <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
--- Frontend/src/models/module/pages/Modules.tsx
+++ Frontend/src/models/module/pages/Modules.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { getAllModules } from "../../../api/ModuleApi";
 import Page from "../../../components/Page";
 import { Link } from "react-router-dom";
+import LoadingMessage from "../../../components/LoadingMessage";
+import ErrorMessage from "../../../components/ErrorMessage";
 
 export default function Modules() {
 
@@ -14,8 +16,8 @@ export default function Modules() {
         <Page>
 
             <section className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 md:px-24 lg:px-56">
-                {isLoading && <p>Loading...</p>}
-                {isError && <p>An error occured...</p>}
+                {isLoading && <LoadingMessage />}
+                {isError && <ErrorMessage />}
                 {data &&
                     <Link to={"/modules/create"} className="border border-black bg-primary text-white pb-[100%] relative">
                         <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
