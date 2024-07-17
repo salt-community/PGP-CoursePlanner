0ab26bdf1c07a51e6d9541ba42739d137d190872
Ewy Rostam <ewy.rost@gmail.com>
2024-07-17 09:39:56 +0200

    refactor: remove unneccassary div

--- Frontend/src/models/course/pages/CourseDetails.tsx
+++ Frontend/src/models/course/pages/CourseDetails.tsx
@@ -94,86 +94,84 @@ export default function CourseDetails() {
             }
         }
     }
-        
-        const queryClient = useQueryClient();
-        const mutation = useMutation({
-            mutationFn: (id: number) => {
-                return deleteCourse(id);
-            },
-            onSuccess: () => {
-                queryClient.invalidateQueries({ queryKey: ['courses'] })
-                navigate(`/courses`);
-            }
-        })
 
-        return (
-            <Page>
-                {isLoading && <LoadingMessage/>}
-                {isError && <ErrorMessage/>}
-                {course &&
-                    <section className="mx-auto flex flex-col gap-4 px-4 md:px-24 lg:px-56">
-                        <div className="">
-                            <section className="flex items-center flex-col gap-4 px-1 sm:p-0">
-                                <h1 className="pb-4 text-xl text-primary font-bold">{course.name}</h1>
-                                {modules.map((module, index) =>
-                                    <div key={module.id}>
-                                        <h1 className="text-lg text-black font-bold self-start">
-                                            <Link to={`/modules/details/${module.id}`}>
-                                                Module {index + 1}: {module.name}
-                                            </Link>
-                                        </h1>
-                                        <table className="table table-fixed table-sm lg:table-lg" key={"module_" + index}>
-                                            <thead>
-                                                <tr>
-                                                    <th className="text-sm w-1/6">Day</th>
-                                                    <th className="text-sm w-1/6">Events</th>
-                                                    <th className="text-sm w-2/3">Description</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
+    const queryClient = useQueryClient();
+    const mutation = useMutation({
+        mutationFn: (id: number) => {
+            return deleteCourse(id);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['courses'] })
+            navigate(`/courses`);
+        }
+    })
 
-                                                {module.days.map((day, dayIndex) =>
-                                                    <tr key={dayIndex}>
-                                                        <td className="text-sm">{day.dayNumber}</td>
-                                                        <td className="text-sm">{day.events.length}</td>
-                                                        <td className="text-sm">{day.description}</td>
-                                                    </tr>
-                                                )}
-                                                <tr></tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                )}
+    return (
+        <Page>
+            {isLoading && <LoadingMessage />}
+            {isError && <ErrorMessage />}
+            {course &&
+                <section className="mx-auto flex flex-col gap-4 px-4 md:px-24 lg:px-56">
+                    <section className="flex items-center flex-col gap-4 px-1 sm:p-0">
+                        <h1 className="pb-4 text-xl text-primary font-bold">{course.name}</h1>
+                        {modules.map((module, index) =>
+                            <div key={module.id}>
+                                <h1 className="text-lg text-black font-bold self-start">
+                                    <Link to={`/modules/details/${module.id}`}>
+                                        Module {index + 1}: {module.name}
+                                    </Link>
+                                </h1>
+                                <table className="table table-fixed table-sm lg:table-lg" key={"module_" + index}>
+                                    <thead>
+                                        <tr>
+                                            <th className="text-sm w-1/6">Day</th>
+                                            <th className="text-sm w-1/6">Events</th>
+                                            <th className="text-sm w-2/3">Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
 
-                            </section>
-                        </div>
+                                        {module.days.map((day, dayIndex) =>
+                                            <tr key={dayIndex}>
+                                                <td className="text-sm">{day.dayNumber}</td>
+                                                <td className="text-sm">{day.events.length}</td>
+                                                <td className="text-sm">{day.description}</td>
+                                            </tr>
+                                        )}
+                                        <tr></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
+
+                    </section>
 
                     <div className="pt-4 flex gap-4 flex-col sm:flex-row">
-                    {usedCourses.find(c => c == course.id) 
-                    ? <Popup
-                        open={isOpenedDelete}
-                        onOpen={() => setIsOpenedDelete(true)}
-                        trigger={<DeleteBtn onClick={() => {}} >Delete Course</DeleteBtn>}
-                        modal
-                    >
-                        {
-                            <div ref={popupRef}>
-                                <div className="flex flex-col">
-                                    <div className="flex justify-end">
-                                        <CloseBtn onClick={() => setIsOpenedDelete(false)} />
+                        {usedCourses.find(c => c == course.id)
+                            ? <Popup
+                                open={isOpenedDelete}
+                                onOpen={() => setIsOpenedDelete(true)}
+                                trigger={<DeleteBtn onClick={() => { }} >Delete Course</DeleteBtn>}
+                                modal
+                            >
+                                {
+                                    <div ref={popupRef}>
+                                        <div className="flex flex-col">
+                                            <div className="flex justify-end">
+                                                <CloseBtn onClick={() => setIsOpenedDelete(false)} />
+                                            </div>
+                                            <h1 className="m-2">This course is used in the calendar. Deleting it will remove all calendar entries using this course.</h1>
+                                            <h1 className="font-bold m-2">Do you want to continue?</h1>
+                                            <div className="flex items-center justify-center mb-4 gap-2">
+                                                <input onClick={() => mutation.mutate(parseInt(courseId))} className="btn btn-sm mt-4 w-24 btn-success text-white" value={"Yes"} />
+                                                <input className="btn btn-sm mt-4 w-24 btn-error text-white" value={"No"} onClick={() => setIsOpenedDelete(false)} />
+                                            </div>
+                                        </div>
                                     </div>
-                                    <h1 className="m-2">This course is used in the calendar. Deleting it will remove all calendar entries using this course.</h1>
-                                    <h1 className="font-bold m-2">Do you want to continue?</h1>
-                                    <div className="flex items-center justify-center mb-4 gap-2">
-                                        <input onClick={() => mutation.mutate(parseInt(courseId))} className="btn btn-sm mt-4 w-24 btn-success text-white" value={"Yes"} />
-                                        <input className="btn btn-sm mt-4 w-24 btn-error text-white" value={"No"} onClick={() => setIsOpenedDelete(false)} />
-                                    </div>
-                                </div>
-                            </div>
+                                }
+                            </Popup>
+                            : <DeleteBtn onClick={() => mutation.mutate(parseInt(courseId))} >Delete Course</DeleteBtn>
                         }
-                    </Popup>
-                    : <DeleteBtn onClick={() => mutation.mutate(parseInt(courseId))} >Delete Course</DeleteBtn>
-                    }
                         <Link to={`/courses/edit/${courseId}`} className="btn btn-sm py-1 max-w-xs btn-info text-white">Edit Course</Link>
                     </div>
                     <p className="error-message text-red-600 text-sm hidden" id="invalid-module-delete">Cannot delete this course, it is used in the calendar!</p>
@@ -193,29 +191,29 @@ export default function CourseDetails() {
                             }
                         } />
 
-                            <Popup
-                                open={isOpened}
-                                onOpen={() => setIsOpened(true)}
-                                trigger={<ColorBtn color={color}>Select color</ColorBtn>}
-                                modal
-                            >
-                                {
-                                    <div ref={popupRef}>
+                        <Popup
+                            open={isOpened}
+                            onOpen={() => setIsOpened(true)}
+                            trigger={<ColorBtn color={color}>Select color</ColorBtn>}
+                            modal
+                        >
+                            {
+                                <div ref={popupRef}>
 
-                                        <div className="flex flex-col">
-                                            <div className="flex justify-end">
-                                                <CloseBtn onClick={() => setIsOpened(false)} />
-                                            </div>
-                                            <div className="self-center mt-2 mb-4">
-                                                <ColorSelection color={color} setColor={setColor}></ColorSelection>
-                                            </div>
-                                            <div className="self-center mb-4">
-                                                <ColorBtn onClick={() => setIsOpened(false)} color={color}>Select color</ColorBtn>
-                                            </div>
+                                    <div className="flex flex-col">
+                                        <div className="flex justify-end">
+                                            <CloseBtn onClick={() => setIsOpened(false)} />
+                                        </div>
+                                        <div className="self-center mt-2 mb-4">
+                                            <ColorSelection color={color} setColor={setColor}></ColorSelection>
+                                        </div>
+                                        <div className="self-center mb-4">
+                                            <ColorBtn onClick={() => setIsOpened(false)} color={color}>Select color</ColorBtn>
                                         </div>
                                     </div>
-                                }
-                            </Popup>
+                                </div>
+                            }
+                        </Popup>
 
                     </div>
                     {isColorNotSelected &&
