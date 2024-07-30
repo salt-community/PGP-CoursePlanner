fa992441e540abe2e9b7e803f721fe88edcf0f2a
Ewy Rostam <ewy.rost@gmail.com>
2024-07-30 09:45:07 +0200

    Remove comments

--- Backend/Controllers/TokensController.cs
+++ Backend/Controllers/TokensController.cs
@@ -11,7 +11,6 @@ using Newtonsoft.Json;
 
 namespace Backend.Controllers
 {
-    // [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class TokensController : ControllerBase
@@ -22,19 +21,6 @@ namespace Backend.Controllers
         {
             _context = context;
         }
-
-        // private async Task<TokenResponse> GetTokensFromGoogle(string code, string ClientId, string ClientSecret)
-        // {
-        //     var client =  new HttpClient();
-
-        //     var body = new StringContent(content, Encoding.URI, "application/x-www-form-urlencoded");
-        //     body.Headers.ContentType = new MediaTypeHeaderValue("content-type", "application/x-www-form-urlencoded");
-
-
-        //     var response = client.PostAsync("https://accounts.google.com/o/oauth2/token", );
-        //     request.AddParameter("application/x-www-form-urlencoded", "grant_type=authorization_code&client_id={yourClientId}&client_secret=%7ByourClientSecret%7D&code=%7ByourAuthorizationCode%7D&redirect_uri={https://yourApp/callback}", ParameterType.RequestBody);
-        //     IRestResponse response = client.Execute(request);
-        // }
 
         private async Task<ActionResult<TokenResponse>> GetTokensFromGoogle(string Url, string code, string ClientId, string ClientSecret, string redirectUrl)
         {
@@ -180,28 +166,3 @@ namespace Backend.Controllers
 
 
 }
-
-// const response = await fetch(BASE_URL, {
-//       method: "POST",
-//       headers: {
-//         "content-type": "application/x-www-form-urlencoded",
-//       },
-//       body: new URLSearchParams({
-//         grant_type: "authorization_code",
-//         code: auth_code,
-//         client_id: import.meta.env.VITE_APP_CLIENT_ID,
-//         client_secret: import.meta.env.VITE_APP_CLIENT_SECRET,
-//         redirect_uri: "http://localhost:5173",
-//       }),
-//     });
-
-//     if (!response.ok || response == undefined) {
-//       alert("failed get access token");
-//       return;
-//     }
-//     const data = await response.json();
-//     return data as tokenResponse;
-//   } catch (error) {
-//     console.error("Error getting access token", error);
-//     alert("Failed to get access token");
-//   }
