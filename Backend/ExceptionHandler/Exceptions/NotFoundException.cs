0fc139ae89a347517f0fa4ef6800b3e33a3232ce
Ewy Rostam <ewy.rost@gmail.com>
2024-08-01 13:13:13 +0200

    fix: add correct namespace to notfoundexception

--- Backend/ExceptionHandler/Exceptions/NotFoundException.cs
+++ Backend/ExceptionHandler/Exceptions/NotFoundException.cs
@@ -1,16 +1,16 @@
-namespace Backend.ExceptionHandler
+namespace Backend.ExceptionHandler.Exceptions
 {
     public class NotFoundException<T> : Exception
     {
-        public string? ResourceName {get;}
-        public T? ResourceId {get;}
+        public string? ResourceName { get; }
+        public T? ResourceId { get; }
         public NotFoundException()
-        {}
+        { }
 
         public NotFoundException(string message) : base(message)
-        {}
+        { }
         public NotFoundException(string message, Exception inner) : base(message, inner)
-        {}
+        { }
         public NotFoundException(string resourceName, T resourceId) : base($"{resourceName} with ID {resourceId} was not found.")
         {
             ResourceName = resourceName;
