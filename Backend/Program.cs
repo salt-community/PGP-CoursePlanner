2ff999834fbb721dc24308a495e76115c668a93b
Ewy Rostam <ewy.rost@gmail.com>
2024-08-02 11:01:54 +0200

    register http client in program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
