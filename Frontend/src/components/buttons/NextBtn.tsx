f992570113e7ab6a77d92faee141ef58b3029975
Patt1989 <patricia.vanmarlen@appliedtechnology.se>
2024-08-13 11:36:06 +0200

    changes hover previous and next button

--- Frontend/src/components/buttons/NextBtn.tsx
+++ Frontend/src/components/buttons/NextBtn.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function NextBtn({ onClick }: Props) {
     return (
         <div className="h-[100px] flex items-end mr-28">
-            <button onClick={onClick} className="btn btn-square btn-outline">
+            <button onClick={onClick} className="btn btn-square btn-outline hover:bg-gray-200">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
             </button>
         </div>
