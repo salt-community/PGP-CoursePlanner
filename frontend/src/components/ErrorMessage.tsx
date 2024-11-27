1e7251cb29195578ff37dee5a859b09b4fd84df2
timabloom <timabloom@proton.me>
2024-11-27 15:56:46 +0100

    fix: login authentication

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 export default function ErrorMessage () {
     return (
         <div className="w-full h-full flex items-center justify-center flex-col">
-            <p>An error has ocurred. Try refreshing the page.</p>
+            <p>An error has occurred. Try refreshing the page.</p>
         </div>
     )
 }
