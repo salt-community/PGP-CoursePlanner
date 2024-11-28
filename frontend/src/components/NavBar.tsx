4533644ca0cbbfc5ca1af9ffc77850093f43f9d8
timabloom <timabloom@proton.me>
2024-11-28 09:38:47 +0100

    refactor: add delete cookie on logout

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -15,6 +15,7 @@ export default function NavBar() {
   const handleLogOut = () => {
     deleteCookie('JWT');
     deleteCookie("access_token");
+    deleteCookie("auth_code");
     navigate("/");
     window.location.reload();
   };
