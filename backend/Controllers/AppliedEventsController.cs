e4d353f88ab4c8271bf3ffb482382333a494a8a1
calle1212 <ch98@hotmail.se>
2024-11-29 13:28:13 +0100

    fix: fix appliedEventsController by removing IService from it

--- backend/Controllers/AppliedEventsController.cs
+++ backend/Controllers/AppliedEventsController.cs
@@ -1,36 +1,45 @@
 
+using backend.Data;
+using backend.ExceptionHandler.Exceptions;
 using backend.Models;
 using backend.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace backend.Controllers
+namespace backend.Controllers;
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class AppliedEventsController : ControllerBase
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class AppliedEventsController : ControllerBase
+    // private readonly IService<Event> _service; // I removed IService from this controller since it caused errors.
+    private DataContext _context;
+    public AppliedEventsController(DataContext context)
     {
-        private readonly IService<Event> _service;
-
-        public AppliedEventsController(IService<Event> service)
-        {
-            _service = service;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Event>> CreateAppliedEvent(Event appliedEvent)
-        {
-            appliedEvent.IsApplied = true;
-            await _service.CreateAsync(appliedEvent);
-            return CreatedAtAction("GetAppliedEvent", new { id = appliedEvent.Id }, appliedEvent);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Event>> GetAppliedEvent(int id)
-        {
-            return await _service.GetOneAsync(id);
-
-        }
+        _context = context;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Event>> CreateAppliedEvent(Event appliedEvent)
+    {
+        appliedEvent.IsApplied = true;
+
+        _context.ChangeTracker.Clear();
+        _context.Entry(appliedEvent).State = EntityState.Added;
+        await _context.Events.AddAsync(appliedEvent);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetAppliedEvent", new { id = appliedEvent.Id }, appliedEvent);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Event>> GetAppliedEvent(int id)
+    {
+        return await _context.Events.FirstOrDefaultAsync(e => e.Id == id)
+        ?? throw new NotFoundByIdException("Event", id);
+
+    }
+
+
 }
