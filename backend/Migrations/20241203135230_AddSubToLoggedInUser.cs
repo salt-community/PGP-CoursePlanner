94471cb731ed1a003ecffe07f9a3ff089922ccfc
calle1212 <ch98@hotmail.se>
2024-12-03 14:53:23 +0100

    feat: add Sub LoggedInUser with migrations

---
+++ backend/Migrations/20241203135230_AddSubToLoggedInUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubToLoggedInUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sub",
+                table: "LoggedInUser",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sub",
+                table: "LoggedInUser");
+        }
+    }
+}
