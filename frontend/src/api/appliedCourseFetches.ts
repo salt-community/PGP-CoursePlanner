a454f8b4f0beff5dab912e950c64d0e96a124819
timabloom <timabloom@proton.me>
2024-12-04 13:05:39 +0100

    frontend test: setting env in github

--- frontend/src/api/appliedCourseFetches.ts
+++ frontend/src/api/appliedCourseFetches.ts
@@ -1,7 +1,7 @@
 import { getCookie } from "@helpers/cookieHelpers";
 import { CourseType } from "@models/course/Types";
 
-const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/AppliedCourses`;
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/AppliedCourses`; 
 
 export async function getAppliedCourses() {
   const response = await fetch(BASE_URL, {
