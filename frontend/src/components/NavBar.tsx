fe2d41427f9fc509a4b6a633457ebf90f1a42823
calle1212 <ch98@hotmail.se>
2024-12-11 15:20:49 +0100

    feat: move navbar to become a sidebar, kinda buggy for now. more work required

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import PrimaryBtn from "./buttons/PrimaryBtn";
-import { deleteCookie} from "@helpers/cookieHelpers";
+import { deleteCookie } from "@helpers/cookieHelpers";
 import { currentMonth, currentYear } from "@helpers/dateHelpers";
 
 export default function NavBar() {
@@ -22,91 +22,56 @@ export default function NavBar() {
 
   return (
     <>
-      <div className="navbar mb-5 md:mb-10 animate-fade-down border-b">
-        <div className="navbar-start">
-          <div className="dropdown bg-white">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost md:hidden"
-              onClick={() => toggleDropdown}
+      <div className="fixed left-0 top-0 h-full w-64 bg-gray-800 text-white shadow-lg">
+        <div className="p-5 border-b border-gray-700">
+          <Link className="text-xl font-bold text-white" to="/">
+            Course Planner
+          </Link>
+        </div>
+        <ul className="menu menu-vertical p-4 text-lg space-y-2">
+          <li>
+            <Link
+              onClick={toggleDropdown}
+              to={`/calendar/month/monthyear=${currentMonth}-${currentYear}`}
+              className="hover:bg-gray-700 rounded p-2"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${isOpen ? "block" : "hidden"
-                }`}
-            >
-              <li>
-                <Link onClick={() => toggleDropdown()} to={`/calendar/month/monthyear=${currentMonth}-${currentYear}`}>
-                  Course Templates
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => toggleDropdown()} to="/modules">
-                  Module Templates
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => toggleDropdown()} to="/courses">
-                  Course Planner
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => toggleDropdown()} to="/activecourses">
-                  Bootcamps
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <Link className="btn btn-ghost text-l text-xl" to="/">
-              Course Planner
+              Calendar
             </Link>
-          </div>
+          </li>
+          <li>
+            <Link
+              onClick={toggleDropdown}
+              to="/modules"
+              className="hover:bg-gray-700 rounded p-2"
+            >
+              Module Templates
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={toggleDropdown}
+              to="/courses"
+              className="hover:bg-gray-700 rounded p-2"
+            >
+              Course Templates
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={toggleDropdown}
+              to="/activecourses"
+              className="hover:bg-gray-700 rounded p-2"
+            >
+              Bootcamps
+            </Link>
+          </li>
+        </ul>
+        <div className="p-4 border-t border-gray-700">
+          <PrimaryBtn onClick={handleLogOut} >
+            Log Out
+          </PrimaryBtn>
         </div>
-        <div className="navbar-center hidden md:flex">
-          <ul className="menu menu-horizontal px-1 text-xl">
-            <li>
-              <Link onClick={() => toggleDropdown()} to={`/calendar/month/monthyear=${currentMonth}-${currentYear}`}>
-                Calendar
-              </Link>
-            </li>
-            <li>
-              <Link onClick={() => toggleDropdown()} to="/modules">
-                Module Templates
-              </Link>
-            </li>
-            <li>
-              <Link onClick={() => toggleDropdown()} to="/courses">
-                Course Templates
-              </Link>
-            </li>
-            <li>
-              <Link onClick={() => toggleDropdown()} to="/activecourses">
-                Bootcamps
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <PrimaryBtn onClick={handleLogOut}>Log Out</PrimaryBtn>
-        </div>
-      </div >
+      </div>
     </>
-  )
+  );
 }
