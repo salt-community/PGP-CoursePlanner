205aa88518608671ad1f42fc3ce89e8d1822f6f6
StefanStenStark <Stefan.frank.sund@gmail.com>
2024-12-12 22:13:01 +0100

    feat: updated types

--- frontend/src/models/course/Types.ts
+++ frontend/src/models/course/Types.ts
@@ -1,15 +1,56 @@
-export type CourseType = {
+export type EventType = {
+    id: number;
+    name: string;
+    startTime: string; 
+    endTime: string; 
+    description?: string;
+    isApplied?: boolean;
+  }
+  
+  export type DayType = {
+    id: number;
+    dayNumber: number;
+    description?: string;
+    isApplied: boolean;
+    events: EventType[];
+  }
+  
+  export type ModuleType = {
+    id: number;
+    name: string;
+    numberOfDays: number;
+    track: string[];
+    order: number;
+    isApplied: boolean;
+    days: DayType[]; 
+   
+    courseId?: number;  
+    moduleId?: number; 
+    module?: {
+      id: number;
+      name: string;
+      track: string[];
+      order: number;
+      isApplied: boolean;
+      numberOfDays: number;
+      days: DayType[];
+    }; 
+  }
+  
+  export type CourseType = {
     id?: number;
     name: string;
     startDate: Date;
     endDate?: Date;
     numberOfWeeks?: number;
-    moduleIds?: number[];
+    moduleIds?: number[]; 
     color?: string;
     isApplied?: boolean;
-}
-
-export type CourseProps = {
+    modules: ModuleType[]; 
+  }
+  
+  export type CourseProps = {
     course: CourseType;
     buttonText: string;
-}
+  }
+  
