6019a58bc33e05c1355681c6a7754117310732b5
calle1212 <ch98@hotmail.se>
2024-12-16 13:30:09 +0100

    fix: update to use new colors for login

--- frontend/src/models/login/Login.tsx
+++ frontend/src/models/login/Login.tsx
@@ -18,7 +18,7 @@ export default function Login() {
       <button
         type="button"
         onClick={() => handleLogin()}
-        className="text-white w-full  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2 max-w-sm">
+        className="text-white w-full  bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none focus:ring-primary/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2 max-w-sm">
         <svg
           className="mr-2 -ml-1 w-4 h-4"
           aria-hidden="true"
