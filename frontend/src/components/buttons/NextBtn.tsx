82a062c9e5334d5fa60cf216f0d8df3f7b748383
calle1212 <ch98@hotmail.se>
2024-12-17 14:56:18 +0100

    fix: basic layout for calendar

--- frontend/src/components/buttons/NextBtn.tsx
+++ frontend/src/components/buttons/NextBtn.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function NextBtn({ onClick }: Props) {
     return (
-            <button onClick={onClick} className="btn btn-square flex mt-72">
+            <button onClick={onClick} className="btn btn-square flex">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" strokeWidth="2" stroke="currentColor"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
             </button>
     )
--- frontend/src/models/calendar/pages/MonthView.tsx
+++ frontend/src/models/calendar/pages/MonthView.tsx
@@ -3,8 +3,8 @@ import PreviousBtn from "@components/buttons/PreviousBtn"
 import Page from "@components/Page"
 import { useState } from "react"
 import CalendarDate from "../sections/CalendarDate"
-import { Link, useNavigate } from "react-router-dom"
-import { currentMonth, firstDayOfMonth, allDaysInInterval, currentYear, fullWeek, daysBeforeMonth, firstWeekDay, getDateAsString, lastDayOfMonth, today } from "../../../helpers/dateHelpers"
+import {  useNavigate } from "react-router-dom"
+import { firstDayOfMonth, allDaysInInterval, currentYear, fullWeek, daysBeforeMonth, firstWeekDay, getDateAsString, lastDayOfMonth, today } from "../../../helpers/dateHelpers"
 import { format, getMonth, getWeek, getYear } from "date-fns"
 import { useMonthFromPath, useYearFromPath } from "@helpers/helperHooks"
 import { useQueryCalendarDateBatch } from "@api/calendarDate/calendarDateQueries"
@@ -32,29 +32,39 @@ export default function MonthView() {
     const startOfMonth2 = getDateAsString(startOfMonth);
     const endOfMonth2 = getDateAsString(endOfMonth);
 
-    const {data, isPending, isError, error} = useQueryCalendarDateBatch(startOfMonth2, endOfMonth2);
+    const { data, isPending, isError, error } = useQueryCalendarDateBatch(startOfMonth2, endOfMonth2);
 
     if (isError) {
         console.log("Query error:", error);
     }
-    if (isPending) return "pending"
+    if (isPending) return "pending";
 
     return (
-        <Page>
-            <section className="flex pb-10">
-                <div className="flex w-1/6 justify-around">
+        <Page >
+            <header className="flex mb-0 p-0 items-center gap-2">
+                <div className="flex items-center">
                     <PreviousBtn onClick={() => { setMonth(month - 1); navigate(`/calendar/month/monthyear=${month - 1}-${year}`); }} />
+                    <NextBtn onClick={() => { setMonth(month + 1); navigate(`/calendar/month/monthyear=${month + 1}-${year}`); }} />
                 </div>
-                <div className="flex flex-col items-center w-4/6">
-                    <header className="mt-5 mb-5">
-                        <h1 className="text-3xl">
-                            {monthInText} {year}
-                        </h1>
-                    </header>
+                <h1 className="text-3xl">{monthInText} {year}</h1>
+                <select className="select select-bordered select-sm max-w-xs ">
+                    <option disabled selected>Month</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+                <label> Filter Tracks</label>
+                <select className="select select-bordered select-sm max-w-xs">
+                    <option disabled selected>All</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+            </header>
+
+            <section className="flex pb-1 flex-grow overflow-y-auto">
+                <div className="flex flex-col items-center w-full h-full">
                     <div className={`justify-center w-full shadow-xl drop-shadow-2xl break-normal grid grid-cols-7 ${numberOfRows} rounded-md bg-white`}>
                         {fullWeek.map(day => (
-                            <div key={format(day, 'E')} className="h-24 w-1/7 flex items-center justify-center py-1 px-1 border-b-2 border-gray-100 border-3">{format(day, 'E')}
-                            </div>
+                            <div key={format(day, 'E')} className="h-24 w-1/7 flex items-center justify-center py-1 px-1 border-b-2 border-gray-100 border-3">{format(day, 'E')}</div>
                         ))}
                         {daysBeforeMonth(startOfMonth, firstWeekDay(startOfMonth)).map((emptyDayIndex) => (
                             <div key={format(emptyDayIndex, 'd')} className="w-1/7 h-24"></div>
@@ -64,22 +74,8 @@ export default function MonthView() {
                                 {data && data[dateIndex] !== null ? <CalendarDate dateContent={data[dateIndex].dateContent} key={format(thisDate, 'd')} date={getDateAsString(thisDate)} />
                                     : <CalendarDate dateContent={[]} key={format(thisDate, 'd')} date={getDateAsString(thisDate)} />}
                             </div>
-                        })
-                        }
+                        })}
                     </div>
-                    <div className="flex flex-row gap-2">
-                        {currentMonth == month && currentYear == year
-                            ? <Link to={`/calendar/week/weeknumberyear=${getWeek(today)}-${year}`} className="btn btn-sm py-1 mt-4 max-w-xs btn-info text-white">Go to week view</Link>
-                            : <>{startOfMonth.getDay() == 0
-                                ? <Link to={`/calendar/week/weeknumberyear=${getWeek(startOfMonth) - 1}-${year}`} className="btn btn-sm py-1 mt-4 max-w-xs btn-info text-white">Go to week view</Link>
-                                : <Link to={`/calendar/week/weeknumberyear=${getWeek(startOfMonth)}-${year}`} className="btn btn-sm py-1 mt-4 max-w-xs btn-info text-white">Go to week view</Link>
-                            }
-                            </>}
-                        <Link to={`/calendar/timeline`} className="btn btn-sm py-1 mt-4 max-w-xs btn-info text-white">Go to timeline</Link>
-                    </div>
-                </div>
-                <div className="flex w-1/6 justify-around">
-                    <NextBtn onClick={() => { setMonth(month + 1); navigate(`/calendar/month/monthyear=${month + 1}-${year}`); }} />
                 </div>
             </section>
         </Page>
