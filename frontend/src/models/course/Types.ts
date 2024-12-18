4a72af65a295ca831c1619d3649bcfd8a5c02ae6
StefanStenStark <Stefan.frank.sund@gmail.com>
2024-12-18 10:19:09 +0100

    feat: updated types to work better with backend

--- frontend/src/models/course/Types.ts
+++ frontend/src/models/course/Types.ts
@@ -1,53 +1,51 @@
+
+export type CourseModuleType = {
+  courseId: number;
+  moduleId: number;
+  course: CourseType;  
+  module: ModuleType;  
+}
+
 export type EventType = {
-    id: number;
-    name: string;
-    startTime: string; 
-    endTime: string; 
-    description?: string;
-    isApplied?: boolean;
-  }
-  
-  export type DayType = {
-    id: number;
-    dayNumber: number;
-    description?: string;
-    isApplied: boolean;
-    events: EventType[];
-  }
-  
-  export type ModuleType = {
-    id: number;
-    name: string;
-    numberOfDays: number;
-    track: string[];
-    order: number;
-    isApplied: boolean;
-    days: DayType[]; 
-   
-    courseId?: number;  
-    moduleId?: number; 
-    module?: {
-      id: number;
-      name: string;
-      track: string[];
-      order: number;
-      isApplied: boolean;
-      numberOfDays: number;
-      days: DayType[];
-    }; 
-  }
-  
-  export type CourseType = {
-    id?: number;
-    name: string;
-    startDate: Date;
-    endDate?: Date;
-    numberOfWeeks?: number;
-    moduleIds?: number[]; 
-    color?: string;
-    isApplied?: boolean;
-    modules: ModuleType[]; 
-  }
+  id: number;
+  name: string;
+  startTime: string;
+  endTime: string;
+  description?: string;
+  isApplied?: boolean;
+}
+
+export type DayType = {
+  id: number;
+  dayNumber: number;
+  description?: string;
+  isApplied: boolean;
+  events: EventType[];
+}
+
+export type ModuleType = {
+  id: number;
+  name: string;
+  numberOfDays: number;
+  track: string[];
+  order: number;
+  isApplied: boolean;
+  days: DayType[];
+  courseModules: CourseModuleType[]; 
+}
+
+export type CourseType = {
+  id?: number;
+  name: string;
+  startDate: Date;
+  endDate?: Date;
+  numberOfWeeks?: number;
+  moduleIds?: number[]; 
+  color?: string;
+  isApplied?: boolean;
+  modules: CourseModuleType[]; 
+}
+
   
   export type CourseProps = {
     course: CourseType;
