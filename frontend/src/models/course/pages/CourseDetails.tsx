62a74672265fa3979183c1a522538bd899274a4f
calle1212 <ch98@hotmail.se>
2025-01-03 11:09:23 +0100

    fix: work on layout

--- frontend/src/models/course/pages/CourseDetails.tsx
+++ frontend/src/models/course/pages/CourseDetails.tsx
@@ -4,6 +4,8 @@ import { useIdFromPath } from "@helpers/helperHooks";
 import { useQueryCourseById, useQueryModulesByCourseId } from "@api/course/courseQueries";
 import LoadingMessage from "@components/LoadingMessage";
 import ErrorMessage from "@components/ErrorMessage";
+import DeleteBtn from "@components/buttons/DeleteBtn";
+import { Link } from "react-router-dom";
 
 export default function CourseDetails() {
   const courseId = useIdFromPath();
@@ -24,19 +26,19 @@ export default function CourseDetails() {
     <Page>
       <h1 className="text-4xl pl-5">Course</h1>
 
-      <section className="grid grid-rows-9 grid-cols-4 h-screen bg-white m-5 rounded-lg overflow-hidden drop-shadow-xl">
+      <section className="grid grid-rows-9 grid-cols-9 h-screen bg-white m-5 rounded-lg overflow-hidden drop-shadow-xl">
         {/* First Row, First Column */}
-        <div className="row-span-1 col-span-1 bg-yellow-500 text-center flex items-center justify-center">
+        <div className="row-span-1 col-span-2 bg-yellow-500 text-center flex items-center justify-center">
           <h2 className="text-3xl">{course?.name || "Course Name"}</h2>
         </div>
 
         {/* First Row, Second Column */}
-        <div className="row-span-1 col-span-3 text-center flex items-center justify-center border-b-2">
+        <div className="row-span-1 col-span-7 text-center flex items-center justify-center border-b-2">
           <h2 className="text-3xl">Modules</h2>
         </div>
 
         {/* Second Row, First Column */}
-        <div className="row-span-8 col-span-1 border-r-2 p-10">
+        <div className="row-span-8 col-span-2 border-r-2 p-10">
           <div className="flex place-content-around p-3 border-b-4  h-20">
             <div className="flex flex-col items-center"><h3>3</h3> <p>Modules</p></div>
             <div className="flex flex-col items-center"><h3>10</h3> <p>Days</p></div>
@@ -49,7 +51,7 @@ export default function CourseDetails() {
         </div>
 
         {/* Second Row, Second Column */}
-        <div className="row-span-8 col-span-3">
+        <div className="row-span-8 col-span-7">
           <div className="h-5/6 overflow-scroll">
             {modules && (
               <>
@@ -60,8 +62,8 @@ export default function CourseDetails() {
             )}
           </div>
           <div className="flex pl-14 pt-4 gap-6">
-            <button className="btn btn-secondary" >Edit Course</button>
-            <button className="btn bg-red-500 text-accent">Delete Course</button>
+            <Link  to={`/courses/edit/${courseId}`} className="btn btn-secondary" >Edit Course</Link>
+            <DeleteBtn onClick={() => console.log("you clicked delete")}>Delete Course</DeleteBtn>
 
             <p> Track: {course?.name}</p>
             <div
