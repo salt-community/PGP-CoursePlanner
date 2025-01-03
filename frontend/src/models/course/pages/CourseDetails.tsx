18a9fbfd7a980dc4e52ea0b775ec885555b60571
calle1212 <ch98@hotmail.se>
2025-01-03 11:12:14 +0100

    feat: make sure course and modules are loaded in

--- frontend/src/models/course/pages/CourseDetails.tsx
+++ frontend/src/models/course/pages/CourseDetails.tsx
@@ -26,58 +26,62 @@ export default function CourseDetails() {
     <Page>
       <h1 className="text-4xl pl-5">Course</h1>
 
-      <section className="grid grid-rows-9 grid-cols-9 h-screen bg-white m-5 rounded-lg overflow-hidden drop-shadow-xl">
-        {/* First Row, First Column */}
-        <div className="row-span-1 col-span-2 bg-yellow-500 text-center flex items-center justify-center">
-          <h2 className="text-3xl">{course?.name || "Course Name"}</h2>
-        </div>
+      {course && modules &&
 
-        {/* First Row, Second Column */}
-        <div className="row-span-1 col-span-7 text-center flex items-center justify-center border-b-2">
-          <h2 className="text-3xl">Modules</h2>
-        </div>
-
-        {/* Second Row, First Column */}
-        <div className="row-span-8 col-span-2 border-r-2 p-10">
-          <div className="flex place-content-around p-3 border-b-4  h-20">
-            <div className="flex flex-col items-center"><h3>3</h3> <p>Modules</p></div>
-            <div className="flex flex-col items-center"><h3>10</h3> <p>Days</p></div>
-            <div className="flex flex-col items-center"><h3>2</h3> <p>Weeks</p></div>
+        <section className="grid grid-rows-9 grid-cols-9 h-screen bg-white m-5 rounded-lg overflow-hidden drop-shadow-xl">
+          {/* First Row, First Column */}
+          <div className="row-span-1 col-span-2 bg-yellow-500 text-center flex items-center justify-center">
+            <h2 className="text-3xl">{course?.name || "Course Name"}</h2>
           </div>
 
-          <div className="p-7 text-center">
-            <h3 className="text-xl">Module Timeline</h3>
+          {/* First Row, Second Column */}
+          <div className="row-span-1 col-span-7 text-center flex items-center justify-center border-b-2">
+            <h2 className="text-3xl">Modules</h2>
           </div>
-        </div>
 
-        {/* Second Row, Second Column */}
-        <div className="row-span-8 col-span-7">
-          <div className="h-5/6 overflow-scroll">
-            {modules && (
-              <>
-                {modules.map((modulemap, index) =>
-                  <ModuleDetails module={modulemap} key={index} />
-                )}
-              </>
-            )}
+          {/* Second Row, First Column */}
+          <div className="row-span-8 col-span-2 border-r-2 p-10">
+            <div className="flex place-content-around p-3 border-b-4  h-20">
+              <div className="flex flex-col items-center"><h3>3</h3> <p>Modules</p></div>
+              <div className="flex flex-col items-center"><h3>10</h3> <p>Days</p></div>
+              <div className="flex flex-col items-center"><h3>2</h3> <p>Weeks</p></div>
+            </div>
+
+            <div className="p-7 text-center">
+              <h3 className="text-xl">Module Timeline</h3>
+            </div>
           </div>
-          <div className="flex pl-14 pt-4 gap-6">
-            <Link  to={`/courses/edit/${courseId}`} className="btn btn-secondary" >Edit Course</Link>
-            <DeleteBtn onClick={() => console.log("you clicked delete")}>Delete Course</DeleteBtn>
 
-            <p> Track: {course?.name}</p>
-            <div
-              style={{
-                width: "15px",
-                height: "15px",
-                backgroundColor: course?.color,
-                borderRadius: "3px",
-              }}
-            > </div>
+          {/* Second Row, Second Column */}
+          <div className="row-span-8 col-span-7">
+            <div className="h-5/6 overflow-scroll">
+              {modules && (
+                <>
+                  {modules.map((modulemap, index) =>
+                    <ModuleDetails module={modulemap} key={index} />
+                  )}
+                </>
+              )}
+            </div>
+            <div className="flex pl-14 pt-4 gap-6">
+              <Link to={`/courses/edit/${courseId}`} className="btn btn-secondary" >Edit Course</Link>
+              <DeleteBtn onClick={() => console.log("you clicked delete")}>Delete Course</DeleteBtn>
 
+              <p> Track: {course?.name}</p>
+              <div
+                style={{
+                  width: "15px",
+                  height: "15px",
+                  backgroundColor: course?.color,
+                  borderRadius: "3px",
+                }}
+              > </div>
+
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      }
+
     </Page>
   );
 }
