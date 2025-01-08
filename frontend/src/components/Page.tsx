473e2341dd5da3f2177e41802e1442840952d2d3
timabloom <timabloom@proton.me>
2025-01-08 14:29:34 +0100

    fix: layout

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -6,41 +6,48 @@ import { useEffect, useState } from "react";
 
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 
 export default function Page({ children }: Props) {
-    const [isSidebarExpanded, setIsSidebarExpanded] = useState(() => {
-      const savedState = localStorage.getItem("isSidebarExpanded");
-      return savedState !== null ? JSON.parse(savedState) : true;
-    });
-  
-    useEffect(() => {
-      localStorage.setItem("isSidebarExpanded", JSON.stringify(isSidebarExpanded));
-    }, [isSidebarExpanded]);
-  
-    useQueryToken();
-  
-    if (location.search) {
-      const authCode = new URLSearchParams(location.search).get('code');
-      if (authCode !== null) {
-        setCookie("auth_code", authCode);
-      }
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(() => {
+    const savedState = localStorage.getItem("isSidebarExpanded");
+    return savedState !== null ? JSON.parse(savedState) : true;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isSidebarExpanded", JSON.stringify(isSidebarExpanded));
+  }, [isSidebarExpanded]);
+
+  useQueryToken();
+
+  if (location.search) {
+    const authCode = new URLSearchParams(location.search).get('code');
+    if (authCode !== null) {
+      setCookie("auth_code", authCode);
     }
-  
-    return (
-      <>
-        {!getCookie("auth_code") && (!getCookie("JWT") || !getCookie("access_token"))
-          ? <Login />
-          : (
-            <div >
-              <NavBar isSidebarExpanded={isSidebarExpanded} setIsSidebarExpanded={setIsSidebarExpanded} />
-              <div className={`transition-all duration-200 ${isSidebarExpanded ? 'ml-64' : 'ml-20'} flex-1 p-4 h-screen flex flex-col`}>
-                {children}
-              </div>
-            </div>
-          )}
-      </>
-    );
   }
+
+  return (
+    <>
+      {!getCookie("auth_code") && (!getCookie("JWT") || !getCookie("access_token"))
+        ? <Login />
+        : (
+          <>
+            <NavBar isSidebarExpanded={isSidebarExpanded} setIsSidebarExpanded={setIsSidebarExpanded} />
+            <header className="absolute top-0 right-0 flex justify-end p-8 pb-2 mr-5">
+              <button className="btn btn-secondary text-xl min-h-10 h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                </svg>
+                Reminder</button>
+            </header>
+            <div className={`transition-all duration-200 ${isSidebarExpanded ? 'ml-64' : 'ml-20'} flex-1 p-4 h-screen flex flex-col`}>
+              {children}
+            </div>
+          </>
+        )}
+    </>
+  );
+}
--- frontend/src/models/home/sections/WeeksContainer.tsx
+++ frontend/src/models/home/sections/WeeksContainer.tsx
@@ -1,7 +1,6 @@
 import LoadingMessage from "@components/LoadingMessage"
 import { getCookie } from "@helpers/cookieHelpers"
-import { currentMonth, currentWeek, currentYear } from "@helpers/dateHelpers"
-import { Link } from "react-router-dom"
+import { currentWeek} from "@helpers/dateHelpers"
 import { useQueryCalendarDateWeeks } from "@api/calendarDate/calendarDateQueries"
 import Week from "./Week"
 
@@ -10,25 +9,21 @@ export default function WeeksContainer() {
     const { data, isLoading: isCalendarLoading } = useQueryCalendarDateWeeks(currentWeek);
 
     return (
-        <section className="pl-20 pr-20 pb-20 flex flex-col items-center">
-            <h2 className="text-2xl font-semibold">Current Week</h2>
+        <section className="pl-10 pr-10 pb-4 pt-6 flex flex-col items-center">
+            <h2 className="self-start text-3xl font-semibold mb-2">Current Week</h2>
             {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
                 ?
                 <LoadingMessage />
                 :
                 <Week data={data} isNextWeek={false} />
             }
-            <h2 className="text-2xl font-semibold">Next Week</h2>
+            <h2 className="self-start text-3xl font-semibold mb-2 mt-4">Next Week</h2>
             {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
                 ?
                 <LoadingMessage />
                 :
                 <Week data={data} isNextWeek={true} />
             }
-            <div className="flex flex-row gap-2">
-                <Link to={`/calendar/month/monthyear=${currentMonth}-${currentYear}`} className="btn  btn-secondary">Go to calendar</Link>
-                <Link to={`/calendar/timeline`} className="btn btn-secondary">Go to timeline</Link>
-            </div>
         </section>
     )
 }
