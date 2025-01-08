eb33b9ab9f944d89192b455c611c828261c17a7b
timabloom <timabloom@proton.me>
2025-01-08 15:49:44 +0100

    feat: implement header

--- frontend/src/models/home/sections/WeeksContainer.tsx
+++ frontend/src/models/home/sections/WeeksContainer.tsx
@@ -1,29 +1,34 @@
 import LoadingMessage from "@components/LoadingMessage"
 import { getCookie } from "@helpers/cookieHelpers"
-import { currentWeek} from "@helpers/dateHelpers"
+import { currentWeek } from "@helpers/dateHelpers"
 import { useQueryCalendarDateWeeks } from "@api/calendarDate/calendarDateQueries"
 import Week from "./Week"
+import Header from "@components/Header"
 
 export default function WeeksContainer() {
 
     const { data, isLoading: isCalendarLoading } = useQueryCalendarDateWeeks(currentWeek);
 
     return (
-        <section className="pl-10 pr-10 pb-4 pt-6 flex flex-col items-center">
-            <h2 className="self-start text-3xl font-semibold mb-2">Current Week</h2>
-            {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
-                ?
-                <LoadingMessage />
-                :
-                <Week data={data} isNextWeek={false} />
-            }
-            <h2 className="self-start text-3xl font-semibold mb-2 mt-4">Next Week</h2>
-            {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
-                ?
-                <LoadingMessage />
-                :
-                <Week data={data} isNextWeek={true} />
-            }
-        </section>
+        <>
+            <Header>
+                <h2 className="text-3xl font-semibold">Current Week</h2>
+            </Header>
+            <section className="pl-10 pr-10 pb-4 flex flex-col items-center">
+                {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
+                    ?
+                    <LoadingMessage />
+                    :
+                    <Week data={data} isNextWeek={false} />
+                }
+                <h2 className="self-start text-3xl font-semibold mt-4 mb-5">Next Week</h2>
+                {isCalendarLoading || (!getCookie("JWT") || !getCookie("access_token"))
+                    ?
+                    <LoadingMessage />
+                    :
+                    <Week data={data} isNextWeek={true} />
+                }
+            </section>
+        </>
     )
 }
