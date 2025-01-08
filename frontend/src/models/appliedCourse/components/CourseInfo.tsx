9dbedc35d6c842ca3a9b4a528665231f3b2ad5c6
StefanStenStark <stefan.frank.sund@gmail.com>
2025-01-08 12:37:00 +0100

    feat: adding component

---
+++ frontend/src/models/appliedCourse/components/CourseInfo.tsx
@@ -0,0 +1,38 @@
+import { CourseType } from "@models/course/Types";
+
+interface CourseInfoProps {
+        course: CourseType;
+        setCourse: React.Dispatch<React.SetStateAction<CourseType>>;
+    }
+
+const CourseInfo = ({ course, setCourse }: CourseInfoProps) => {
+    return (
+            <div className="flex flex-row gap-2 flex-1">
+        <div className="flex flex-col gap-2 flex-1">
+            <label className="text-lg font-medium">Course name:</label>
+            <input
+                type="text"
+                value={course.name}
+                onChange={(e) => setCourse({ ...course, name: e.target.value })}
+                className="w-full p-2 border border-gray-300 rounded-md"
+            />
+        </div>
+        <div className="flex flex-col gap-2 ml-auto">
+            <label className="text-lg font-medium">Track:</label>
+            <select
+                value={course.track}
+                onChange={(e) => setCourse({ ...course, track: e.target.value })}
+                className="w-full p-2 border border-gray-300 rounded-md"
+            >
+                <option value="" disabled>Select a track</option>
+                <option value="Track 1">Track 1</option>
+                <option value="Track 2">Track 2</option>
+                <option value="Track 3">Track 3</option>
+            </select>
+        </div>
+    </div>
+
+    );
+};
+
+export default CourseInfo;
