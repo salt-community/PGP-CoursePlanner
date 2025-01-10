16536478e4b0b3db409e49b78349a8ec68a61dfe
calle1212 <ch98@hotmail.se>
2025-01-10 16:35:19 +0100

    feat: add response DTO records for the most used model classes

---
+++ backend/Models/DTOs/CourseResponse.cs
@@ -0,0 +1,30 @@
+namespace backend.Models.DTOs;
+public record CourseResponse
+{
+    public int Id { get; init; }
+    public string? Name { get; init; }
+    public DateTime StartDate { get; init; }
+    public DateTime EndDate { get; init; }
+    public int NumberOfWeeks { get; init; }
+    public List<int> ModuleIds { get; init; } = new();
+    public List<ModuleResponse> Modules { get; init; } = new();
+    public string? Color { get; init; }
+    public bool IsApplied { get; init; }
+
+    // Constructor
+    public CourseResponse(Course course)
+    {
+        Id = course.Id;
+        Name = course.Name;
+        StartDate = course.StartDate;
+        EndDate = course.EndDate;
+        NumberOfWeeks = course.NumberOfWeeks;
+        ModuleIds = course.Modules.Select(cm => cm.ModuleId).ToList();
+        Modules = course.Modules.Select(cm => new ModuleResponse(cm.Module!)).ToList();
+        Color = course.Color;
+        IsApplied = course.IsApplied;
+    }
+
+    // Implicit operator
+    public static implicit operator CourseResponse(Course course) => new(course);
+}
---
+++ backend/Models/DTOs/ModuleResponse.cs
@@ -0,0 +1,27 @@
+namespace backend.Models.DTOs;
+
+public record ModuleResponse
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public int NumberOfDays { get; init; }
+    public List<DayResponse> Days { get; init; } = new();
+    public string[] Track { get; init; } = Array.Empty<string>();
+    public int Order { get; init; }
+    public bool IsApplied { get; init; }
+
+    // Constructor
+    public ModuleResponse(Module module)
+    {
+        Id = module.Id;
+        Name = module.Name;
+        NumberOfDays = module.NumberOfDays;
+        Days = module.Days.Select(day => new DayResponse(day)).ToList();
+        Track = module.Track;
+        Order = module.Order;
+        IsApplied = module.IsApplied;
+    }
+
+    // Implicit operator
+    public static implicit operator ModuleResponse(Module module) => new(module);
+}
