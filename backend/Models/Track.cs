8a9575c7db1996f3a676ed903b19a2403fda0316
calle1212 <ch98@hotmail.se>
2025-01-10 16:01:09 +0100

    feat: create new model class Track.cs

---
+++ backend/Models/Track.cs
@@ -0,0 +1,13 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class Track
+{
+    public int Id;
+    [Required]
+    public string Name {get; set;} = null!;
+    [Required] 
+    public string Color {get; set;} = null!;
+}
