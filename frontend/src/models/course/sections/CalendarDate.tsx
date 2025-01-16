3fa080a286dcd113a4aa18bdaaccb06d5bb326a4
calle1212 <ch98@hotmail.se>
2025-01-16 11:54:49 +0100

    fix: fix the color when hovering on a selected module cell in the calendar

--- frontend/src/models/course/sections/CalendarDate.tsx
+++ frontend/src/models/course/sections/CalendarDate.tsx
@@ -65,7 +65,7 @@ export default function CalenderDate({ dateContent, date, openModal, indexForMod
 
     return (
         <button onClick={isLoading ? () => { } : () => openModal(indexForModal)}
-            className={`${bgBox} ${border} flex flex-col gap-2 p-4 pt-1 pb-2 items-center h-full ${!isLoading ? "hover:bg-[#F9F9F9] hover:cursor-pointer" : "hover:cursor-default"}`}>
+            className={`${bgBox} ${border} flex flex-col gap-2 p-4 pt-1 pb-2 items-center h-full ${!isLoading ? "hover:bg-[#F9F9F9] hover:cursor-pointer" : "hover:cursor-default"} ${isInSelectedModule ? "hover:bg-pink-50" : ""}`}>
             <div className={`${bg} h-10 w-10 rounded-full flex justify-center items-center`}>
                 <h2 className={`${text}`}>
                     {format(date, 'd')}
