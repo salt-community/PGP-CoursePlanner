e360c72167d1d909099351d39d17e4b4a0d5a113
timabloom <timabloom@proton.me>
2025-01-16 16:02:54 +0100

    feat: add visibility property

--- backend/Models/Track.cs
+++ backend/Models/Track.cs
@@ -5,9 +5,10 @@ namespace backend.Models;
 
 public class Track
 {
-    public int Id {get; set;}
+    public int Id { get; set; }
     [Required]
-    public string Name {get; set;} = null!;
-    [Required] 
-    public string Color {get; set;} = null!;
+    public string Name { get; set; } = null!;
+    [Required]
+    public string Color { get; set; } = null!;
+    public bool Visibility { get; set; } = true;
 }
