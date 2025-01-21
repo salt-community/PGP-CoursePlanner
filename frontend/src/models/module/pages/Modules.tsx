a8511afb4b463c5ee7bb8f18a9fb8e45246c6773
timabloom <timabloom@proton.me>
2025-01-21 16:28:18 +0100

    feat: ui/ux modules page

--- frontend/src/models/module/pages/Modules.tsx
+++ frontend/src/models/module/pages/Modules.tsx
@@ -1,29 +1,32 @@
 import Page from "@components/Page";
 import { Link } from "react-router-dom";
 import { useQueryModules } from "@api/module/moduleQueries";
+import Header from "@components/Header";
 
 export default function Modules() {
     const { data: modules } = useQueryModules();
-
+    console.log(modules)
     return (
         <Page>
-            <section className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 md:px-24 lg:px-56">
+            <Header>
+                <h1 className="text-3xl font-semibold">Modules</h1>
+            </Header>
+            <section className="flex flex-wrap gap-8 p-10 pt-0">
                 {modules &&
-                    <Link to={"/modules/create"} className="border border-primary bg-primary text-white pb-[100%] relative">
-                        <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
+                    <Link to={"/modules/create"} className="flex items-center justify-center bg-primary rounded-xl hover:bg-[#EF4E72] hover:cursor-pointer drop-shadow-xl text-white min-h-80 min-w-80">
+                        <div className="text-lg">
                             Create new module
                         </div>
                     </Link>}
                 {modules && modules.map((module, index) =>
-                    <Link to={`/modules/details/${module.id}`} key={module.name + index} className="border border-black pb-[100%] relative">
-                        <div className="absolute inset-0 flex items-center justify-center flex-col gap-1">
-                            <h2 className="text-primary">{module.name}</h2>
-                            <div className="flex gap-2">
-                                {module.tracks.map((t) => {
-                                    return <h3 className="text-sm" key={t.id}>{t.name}</h3>
-                                })}
-                            </div>
+                    <Link to={`/modules/details/${module.id}`} key={module.name + index} className="flex items-center justify-center flex-col gap-2 relative hover:bg-[#F9F9F9] hover:cursor-pointer bg-white rounded-xl drop-shadow-xl min-h-80 min-w-80">
+                        <h2 className="text-primary text-lg">{module.name}</h2>
+                        <div className="flex gap-2">
+                            {module.tracks.map((t) => {
+                                return <h3 className="text-[#636363]" key={t.id}>{t.name}</h3>
+                            })}
                         </div>
+                        <h4 className="absolute text-[#636363] bottom-0 p-8">Creation Date: 2024-01-13</h4>
                     </Link>
                 )}
             </section>
