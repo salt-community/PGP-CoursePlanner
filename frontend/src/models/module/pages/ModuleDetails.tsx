3569c313f2e144f97f6145c367baae96e7fe190e
timabloom <timabloom@proton.me>
2025-01-21 16:40:47 +0100

    fix: remove tracks

--- frontend/src/models/module/pages/ModuleDetails.tsx
+++ frontend/src/models/module/pages/ModuleDetails.tsx
@@ -36,16 +36,12 @@ export default function ModuleDetails() {
         <Page>
             {isLoading && <LoadingMessage />}
             {isError && <ErrorMessage />}
-            {module && module.track &&
+            {module &&
                 <section className="mx-auto flex flex-col gap-4 px-4 md:px-24 lg:px-56">
                     <div className="w-[320px] overflow-scroll sm:w-auto sm:overflow-auto">
                         <section className="flex items-center flex-col gap-4 px-1 sm:p-0">
                             <div className="flex flex-col items-center">
                                 <h1 className="pb-0 text-xl text-primary font-bold">{module.name}</h1>
-                                {module.track.length > 0
-                                    ? <p className="pb-4 text-lg font-medium">Track: {module.track.join(', ')}</p>
-                                    : <p className="pb-4 text-lg font-medium">Track: not selected</p>
-                                }
                             </div>
                             {module.days.map((day) =>
                                 <div className="w-full" key={"day_" + day.dayNumber}>
