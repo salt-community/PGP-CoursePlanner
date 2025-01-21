a589f20ae06169877d97ee8d5664d7302a638040
timabloom <timabloom@proton.me>
2025-01-21 12:02:17 +0100

    fix: track names in one row

--- frontend/src/models/module/pages/Modules.tsx
+++ frontend/src/models/module/pages/Modules.tsx
@@ -1,17 +1,13 @@
 import Page from "@components/Page";
 import { Link } from "react-router-dom";
-import LoadingMessage from "@components/LoadingMessage";
-import ErrorMessage from "@components/ErrorMessage";
 import { useQueryModules } from "@api/module/moduleQueries";
 
 export default function Modules() {
-    const { data: modules, isLoading, isError } = useQueryModules();
-    
+    const { data: modules } = useQueryModules();
+
     return (
         <Page>
             <section className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 md:px-24 lg:px-56">
-                {isLoading && <LoadingMessage />}
-                {isError && <ErrorMessage />}
                 {modules &&
                     <Link to={"/modules/create"} className="border border-primary bg-primary text-white pb-[100%] relative">
                         <div className="absolute inset-0 flex items-center justify-center flex-col gap-4">
@@ -22,9 +18,11 @@ export default function Modules() {
                     <Link to={`/modules/details/${module.id}`} key={module.name + index} className="border border-black pb-[100%] relative">
                         <div className="absolute inset-0 flex items-center justify-center flex-col gap-1">
                             <h2 className="text-primary">{module.name}</h2>
-                            {module.tracks.map((t) => {
-                                return <h3 key={t.id}>{t.name}</h3>
-                            })}
+                            <div className="flex gap-2">
+                                {module.tracks.map((t) => {
+                                    return <h3 className="text-sm" key={t.id}>{t.name}</h3>
+                                })}
+                            </div>
                         </div>
                     </Link>
                 )}
