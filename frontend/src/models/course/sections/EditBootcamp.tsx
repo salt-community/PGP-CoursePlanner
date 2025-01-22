a380750c19a2e5aea70886a0c12401b3df59b3d7
calle1212 <ch98@hotmail.se>
2025-01-22 09:31:12 +0100

    feat: add a toggle for side panel on EditBootcamp

--- frontend/src/models/course/sections/EditBootcamp.tsx
+++ frontend/src/models/course/sections/EditBootcamp.tsx
@@ -28,7 +28,7 @@ export function EditBootcamp({ course }: Props) {
     const mutationPostAppliedCourse = useMutationPostAppliedCourse();
 
     const navigate = useNavigate();
-    
+
     calculateCourseDayDates(course, startDate)
 
     const [previewCourse, setCourse] = useState<CourseType>(course);
@@ -38,6 +38,8 @@ export function EditBootcamp({ course }: Props) {
     const [selectedModule, setSelectedModule] = useState<ModuleType>(previewCourse.modules[0].module);
 
     const [selectedDate, setSelectedDate] = useState<CalendarDateType>({ date: (new Date()), dateContent: [] })
+
+    const [toggle, setToggle] = useState<string>("info")
 
 
     useEffect(() => {
@@ -99,31 +101,37 @@ export function EditBootcamp({ course }: Props) {
 
     return (
         <>
-                <div className="flex flex-col h-[80vh] max-w-full">
+            <div className="flex flex-col h-[80vh] max-w-full">
+                <br />
+                <section className="flex flex-grow">
 
-                    <br />
-                    <section className="flex flex-grow">
-
-                        <div className="flex-grow overflow-auto">
-                            <MiniCalendar previewCourse={previewCourse} startDate={startDate} previewCalendarDays={previewCalendarDays} selectedModule={selectedModule} selectedModuleStartDate={selectedDate} setSelectedModuleStartDate={setSelectedDate} setSelectedModule={setSelectedModule} />
-                        </div>
-                        <InfoPanel selectedModule={selectedModule} selectedDate={selectedDate} handleMoveModule={handleMoveModule} />
-                        <EditCourseDays appliedCourse={course} course={previewCourse} setCourse={setCourse} />
-                    </section>
-                    <div className="modal-action">
-                        <form method="dialog" className="flex gap-5 justify-center" >
-                            <button className="btn">Cancel</button>
-                            <div className="flex flex-col">
-                                <label>add to google calendar<input type="checkbox"  {...register("isDeployingToGoogle", { required: false })}></input></label>
-                                {watch("isDeployingToGoogle") && <label>Group email <input type="email" defaultValue={""} {...register("groupEmail", { required: false })}></input></label>}
-
-                            </div>
-                            <button className="btn btn-primary" type="submit" onClick={handleSubmit(onSubmit)}>Deploy Bootcamp</button>
-
-
-                        </form>
+                    <div className="flex-grow overflow-auto">
+                        <MiniCalendar previewCourse={previewCourse} startDate={startDate} previewCalendarDays={previewCalendarDays} selectedModule={selectedModule} selectedModuleStartDate={selectedDate} setSelectedModuleStartDate={setSelectedDate} setSelectedModule={setSelectedModule} />
                     </div>
+                    <div className="flex flex-col w-2/5">
+                        <div className="flex">
+                            <button className="btn w-1/3" onClick={() => setToggle("info")}>Info</button>
+                            <button className="btn w-1/3" onClick={() => setToggle("edit")}>Edit</button>
+                            <button className="btn w-1/3">Nothing</button>
+                        </div>
+                        {toggle == "info" && <InfoPanel selectedModule={selectedModule} selectedDate={selectedDate} handleMoveModule={handleMoveModule} />}
+                        {toggle == "edit" && <EditCourseDays appliedCourse={course} course={previewCourse} setCourse={setCourse} />}
+                    </div>
+                </section>
+                <div className="modal-action">
+                    <form method="dialog" className="flex gap-5 justify-center" >
+                        <button className="btn">Cancel</button>
+                        <div className="flex flex-col">
+                            <label>add to google calendar<input type="checkbox"  {...register("isDeployingToGoogle", { required: false })}></input></label>
+                            {watch("isDeployingToGoogle") && <label>Group email <input type="email" defaultValue={""} {...register("groupEmail", { required: false })}></input></label>}
+
+                        </div>
+                        <button className="btn btn-primary" type="submit" onClick={handleSubmit(onSubmit)}>Deploy Bootcamp</button>
+
+
+                    </form>
                 </div>
+            </div>
 
         </>
     )
