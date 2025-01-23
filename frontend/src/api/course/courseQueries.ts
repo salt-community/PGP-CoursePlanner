c26022a4ff168a96c7ef82f5c1752dbc68561e17
timabloom <timabloom@proton.me>
2025-01-23 10:53:54 +0100

    fix: add skeleton delay

--- frontend/src/api/course/courseQueries.ts
+++ frontend/src/api/course/courseQueries.ts
@@ -2,6 +2,7 @@ import { CourseType } from "@models/course/Types";
 import { useQuery } from "@tanstack/react-query";
 import { getCourses, getCourseById, getModulesByCourseId } from "./courseFetches";
 import { ModuleType } from "@models/module/Types";
+import { useState } from "react";
 
 export function useQueryCourses() {
     const { data, isLoading, isError } = useQuery<CourseType[]>({
@@ -9,7 +10,14 @@ export function useQueryCourses() {
         queryFn: getCourses
     });
 
-    return { data, isLoading, isError };
+    const [delayedLoading, setDelayedLoading] = useState(isLoading);
+    if (!isLoading) {
+        setTimeout(() => {
+            setDelayedLoading(isLoading);
+        }, 500)
+    }
+
+    return { data: data, isLoading: delayedLoading, isError };
 }
 
 export function useQueryCourseById(id: number) {
@@ -18,7 +26,7 @@ export function useQueryCourseById(id: number) {
         queryFn: () => getCourseById(id)
     });
 
-    return { data, isLoading , isError };
+    return { data, isLoading, isError };
 }
 
 export function useQueryModulesByCourseId(id: number) {
