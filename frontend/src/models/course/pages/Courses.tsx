3ee82c47f8aad64343d15d1b1be6ac25ef988114
timabloom <timabloom@proton.me>
2025-01-23 14:25:03 +0100

    feat: implement UI/UX for bootcamps main page

--- frontend/src/models/course/pages/Courses.tsx
+++ frontend/src/models/course/pages/Courses.tsx
@@ -7,6 +7,7 @@ import Header from "@components/Header";
 import { Fragment, useMemo, useState } from "react";
 import { useMutationDeleteCourse } from "@api/course/courseMutations";
 import LoadingSkeletonCourse from "../components/LoadingSkeletonCourse";
+import CourseCard from "@components/CourseCard";
 
 export default function Courses() {
     const { data: courses, isLoading, isError } = useQueryCourses();
@@ -33,32 +34,7 @@ export default function Courses() {
                 {trackNames.map((trackName) =>
                     <Fragment key={trackName}>
                         <h2 className="text-2xl font-semibold mt-4">{trackName}</h2>
-                        {courses && courses.map((course, index) =>
-                            course.track.name == trackName &&
-                            <div key={index} className="flex justify-between items-center bg-white w-full rounded-md drop-shadow-xl">
-                                {isLoading ?
-                                    <LoadingSkeletonCourse />
-                                    :
-                                    <>
-                                        <Link to={`/courses/details/${course.id}`} key={course.name + index} className={`flex justify-between items-center w-full rounded-l-md ${isLoading ? "cursor-default pointer-events-none" : "hover:bg-[#F9F9F9]"}`}>
-                                            <h3 className="text-lg p-6">
-                                                {course.name}
-                                            </h3>
-                                            <h4 className="text-sm mr-6 text-[#636363]">
-                                                Creation Date: 2024-01-13
-                                            </h4>
-                                        </Link>
-                                        <button
-                                            className="bg-red-600 hover:bg-red-700 p-6 px-7 rounded-r-md"
-                                            onClick={() => course.id && mutationDeleteCourse.mutate(course.id)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-7">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                            </svg>
-                                        </button>
-                                    </>
-                                }
-                            </div>
-                        )}
+                        <CourseCard data={courses} isLoading={isLoading} trackName={trackName} mutationDelete={mutationDeleteCourse} />
                     </Fragment>
                 )}
                 {!courses &&
