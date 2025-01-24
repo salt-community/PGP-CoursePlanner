a2893a0ac37edb28a70af4553f54b38b8b946de8
calle1212 <ch98@hotmail.se>
2025-01-24 11:14:16 +0100

    write new query for refresh token

--- frontend/src/api/user/userQueries.ts
+++ frontend/src/api/user/userQueries.ts
@@ -1,11 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import { getToken, tokenResponse } from "./userFetches";
+import { getToken, refreshToken, tokenResponse } from "./userFetches";
 import { setTokenCookies } from "@helpers/cookieHelpers";
 
 export function useQueryToken() {
     const { data } = useQuery<tokenResponse>({
         queryKey: ['accessCode'],
         queryFn: getToken,
+    })
+    setTokenCookies(data);
+}
+
+
+export function useRefrehToken() {
+    const { data } = useQuery<tokenResponse>({
+        queryKey: ['accessCode'],
+        queryFn: refreshToken,
     })
     setTokenCookies(data);
 }
