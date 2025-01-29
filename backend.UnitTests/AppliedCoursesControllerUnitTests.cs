e78a593137e2159ab0f9e09cb487a1d79918f9f2
timabloom <timabloom@proton.me>
2025-01-29 11:51:04 +0100

    fix: CreateAppliedCourse test

--- backend.UnitTests/AppliedCoursesControllerUnitTests.cs
+++ backend.UnitTests/AppliedCoursesControllerUnitTests.cs
@@ -1,6 +1,8 @@
 using backend.Controllers;
 using backend.Models;
+using backend.Models.DTOs;
 using backend.Services;
+using Castle.Components.DictionaryAdapter.Xml;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -9,24 +11,36 @@ namespace backend.Tests.UnitTests
 {
     public class AppliedCoursesControllerUnitTests
     {
-        readonly Mock<IService<Course>> _mockService = new Mock<IService<Course>>();
+        readonly Mock<IService<Course>> _mockService = new();
 
         [Fact]
-        public async void CreateAppliedCourse_Returns_Ok_AppliedCourse()
+        public async void CreateAppliedCourse_Returns_Created_201()
         {
             // arrange
-            var AppliedCourse = new Course() { StartDate = new DateTime(2024 - 07 - 12) };
-            _mockService.Setup(service => service.CreateAsync(AppliedCourse)).ReturnsAsync(AppliedCourse);
+            var appliedCourse = new Course()
+            {
+                Track = new Track() { Id = 1, Name = "Java", Color = "#D73A24", Visibility = true },
+                Name = "Java",
+                StartDate = new DateTime(2024, 7, 12),
+                EndDate = new DateTime(2024, 8, 23),
+                NumberOfWeeks = 6,
+                Color = "#D73A24",
+                IsApplied = true
+            };
+
+            var expectedResponse = (CourseResponse)appliedCourse;
+
+            _mockService.Setup(service => service.CreateAsync(appliedCourse)).ReturnsAsync(appliedCourse);
             var controller = new AppliedCoursesController(_mockService.Object);
 
             // act
-            var result = await controller.CreateAppliedCourse(AppliedCourse);
-            var resultValue = (result.Result as CreatedAtActionResult)!.Value as Course;
+            var result = (await controller.CreateAppliedCourse(appliedCourse)).Result;
+            var objectResult = (result as ObjectResult)!;
 
             // assert
-            resultValue.Should().NotBeNull();
-            resultValue.Should().BeOfType<Course>();
-            resultValue!.StartDate.Should().Be(new DateTime(2024 - 07 - 12));
+            result.Should().BeOfType<CreatedAtActionResult>();
+            objectResult.Value.Should().BeOfType<CourseResponse>();
+            objectResult.Value.Should().BeEquivalentTo(expectedResponse);
         }
 
         [Fact]
