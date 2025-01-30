f116b4bf59d2d2dcb52afd87bfd280ddd59f49a8
StefanStenStark <stefan.frank.sund@gmail.com>
2025-01-30 09:07:38 +0100

    Setting cookie in helper instead

--- frontend/src/helpers/interceptorHelpers.ts
+++ frontend/src/helpers/interceptorHelpers.ts
@@ -1,11 +1,10 @@
 import { refreshToken } from "@api/user/userFetches";
-import { setTokenCookies } from "./cookieHelpers";
 
 export async function fetchWithRefreshTokenInterceptor(url: string, options: RequestInit) {
     const response = await fetch(url, {...options});
     
     if (response.status === 401) {
-        setTokenCookies(await refreshToken());
+        await refreshToken();
     }
     return response;
 }
