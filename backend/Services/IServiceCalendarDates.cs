2ad7d2ed7836903b9bd356fc237c9e8a211265df
timabloom <timabloom@proton.me>
2025-01-31 10:14:55 +0100

    refactor: implement interface

---
+++ backend/Services/IServiceCalendarDates.cs
@@ -0,0 +1,8 @@
+namespace backend.Services;
+
+public interface IServiceCalendarDates<T>
+{
+    Task<T> GetCalendarDate(DateTime date);
+    // Task<List<T>> GetCalendarDateBatch(DateTime start, DateTime end);
+    // Task<List<T>> GetCalendarDate2Weeks(int weekNumber);
+}
