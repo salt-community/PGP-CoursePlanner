24ba57a4a09f99158055af64a1b50d6e4f05e204
timabloom <timabloom@proton.me>
2025-02-04 16:37:38 +0100

    feat: display number of module days

--- frontend/src/models/course/sections/ModuleDetails.tsx
+++ frontend/src/models/course/sections/ModuleDetails.tsx
@@ -7,8 +7,14 @@ type Props = {
 
 export default function ModuleDetails({ module }: Props) {
     return (
-        <div className="p-12">
-            <h3 className="text-3xl">{module.module.name}</h3>
+        <div className="p-10">
+            <div className="flex justify-between">
+                <h3 className="text-3xl">{module.module.name}</h3>
+                <div className="flex flex-col items-center">
+                    <h3 className="text-2xl font-bold">{module.module.days.length}</h3>
+                    <p>Days</p>
+                </div>
+            </div>
             {module.module.days.map((day, index) => (
                 <div key={index}>
                     <h4 className="text-xl pb-1 pt-4" key={index}>Day {day.dayNumber}</h4>
