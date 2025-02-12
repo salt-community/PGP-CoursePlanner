3b3ddf87bb4db0eb5a3c87aefe77f575c0d0a55e
timabloom <timabloom@proton.me>
2025-02-12 10:09:34 +0100

    feat: add creation date to module and course response

--- backend/Models/DTOs/CourseResponse.cs
+++ backend/Models/DTOs/CourseResponse.cs
@@ -2,19 +2,18 @@ namespace backend.Models.DTOs;
 public record CourseResponse
 {
     public int Id { get; init; }
-    public Track Track {get; init;}
+    public Track Track { get; init; }
     public string? Name { get; init; }
     public DateTime StartDate { get; init; }
     public DateTime EndDate { get; init; }
     public int NumberOfWeeks { get; init; }
-    public List<int> ModuleIds { get; init; } = new();
-    public List<CourseModuleResponse> Modules { get; init; } = new();
-    // public List<CourseModuleResponse> CourseModules { get; init; } = new();
+    public List<int> ModuleIds { get; init; } = [];
+    public List<CourseModuleResponse> Modules { get; init; } = [];
     public string? Color { get; init; }
     public bool IsApplied { get; init; }
-    public List<EventResponse> MiscellaneousEvents {get; init;} = [];
+    public List<EventResponse> MiscellaneousEvents { get; init; } = [];
+    public DateTime CreationDate { get; init; }
 
-    // Constructor
     public CourseResponse(Course course)
     {
         Id = course.Id;
@@ -25,12 +24,11 @@ public record CourseResponse
         NumberOfWeeks = course.NumberOfWeeks;
         ModuleIds = course.Modules.Select(cm => cm.ModuleId).ToList();
         Modules = course.Modules.Select(cm => new CourseModuleResponse(cm)).ToList();
-        // Modules = course.Modules.Select(cm => new ModuleResponse(cm.Module!)).ToList();
         Color = course.Track.Color;
         IsApplied = course.IsApplied;
-        MiscellaneousEvents = course.MiscellaneousEvents.Select(e => (EventResponse) e).ToList();
+        MiscellaneousEvents = course.MiscellaneousEvents.Select(e => (EventResponse)e).ToList();
+        CreationDate = course.CreationDate;
     }
 
-    // Implicit operator
     public static implicit operator CourseResponse(Course course) => new(course);
 }
